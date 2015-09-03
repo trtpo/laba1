a34a9941c0628d76b9b9437222f0c811cde12c4b
Piskun Aleksei <piskun_aleksei@mail.ru>
2015-09-03 18:54:46 +0300

    Added a new type of function (div) and changed form of fractal again

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,21 @@ public class Complex {
     }
 
     /**
+     * Division operation.
+     * @param  b divider
+     * @return this Complex object whose value is this / b
+     */
+
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(a.im * a.im + b.im * b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(a.im * a.im + b.im * b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
