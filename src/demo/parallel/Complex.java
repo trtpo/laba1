d216cf93e4c42c74b87e5faa4a8a2110a1c7ffcf
Yegor Chichin <chichin.egor@gmail.com>
2015-09-03 22:13:36 +0300

    Add Junit tests

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -60,11 +60,18 @@ public class Complex {
         im = imag;
     }
 
+    public double getIm(){
+        return im;
+    }
+    public double getRe(){
+        return re;
+    }
     /**
      * Add operation.
      * @param b summand
      * @return this Complex object whose value is (this + b)
      */
+
     public Complex plus(Complex b) {
         re += b.re;
         im += b.im;
