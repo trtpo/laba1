5421ac120a4dbdba46c84fa5aee1afa9ae724a15
KachanAnton350504 <antonn.lida@mail.ru>
2015-09-04 09:12:23 +0300

    Fractal equation has been changed.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(c).times(c).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -289,7 +289,7 @@ class MandelbrotSetTask extends Task<Long> {
      * @return calculated color of the pixel
      */
     private Color calcPixel(double x, double y) {
-        double re = (minR * (width - x) + x * maxR) / width;
+        double re = (minR * (width - x) + x* maxR) / width;
         double im = (minI * (height - y) + y * maxI) / height;
         Complex calPixel = new Complex(re, im);
         return getColor(calc(calPixel));
