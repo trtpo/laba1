96205d8c16206b43e9ddaffde697ff6894e2b128
Zaitsev Yury <zaitsev.yury.w@gmail.com>
2015-09-06 23:45:24 +0300

    Color scheme update

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(0, 20, 40),
+            Color.rgb(0, 128, 255),
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(0, 128, 255),
+            Color.rgb(0, 50, 100),
+            Color.rgb(0, 128, 255),
+            Color.rgb(0, 25, 50)
         };
         
         /**
