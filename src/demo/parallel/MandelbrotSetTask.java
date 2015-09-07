d28fa39e0bcba10e9e806a337187dc34fbebd709
Nikita Bazylchuk <niktabasylschuk@gmail.com>
2015-09-07 17:09:38 +0300

    The new operating above complex number division, new fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex b = new Complex(15, 2);
         do {
-            c = c.times(c).times(c).times(c).plus(comp);
+            c = c.times(c).times(c).division(b).times(c).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
