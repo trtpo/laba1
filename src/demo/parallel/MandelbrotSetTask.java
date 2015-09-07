9bccba1958314293eb34ef4db621acf0c32aecb9
Dmitry <DUMON1996@yandex.ru>
2015-09-07 17:46:04 +0400

    Colors are changed from default to custom

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(0, 40, 0),
+            Color.GOLD,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.YELLOW,
+            Color.rgb(0, 15, 70),
+            Color.BLUE,
+            Color.rgb(90, 0, 25)
         };
         
         /**
