2171d286a2a37761ec2091c1dd91b2755bc223d7
Kravchenko Viktor Viktorovich <kwr@nxt.ru>
2015-09-07 20:18:11 +0300

    a. Changed gamma

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(0, 0, 20),
+            Color.rgb(0, 80, 40),
+            Color.rgb(30, 0, 180),
+            Color.rgb(70, 0, 155),
+            Color.rgb(200, 0, 0),
+            Color.rgb(0, 170, 100),
+            Color.rgb(0, 120, 200)
         };
         
         /**
