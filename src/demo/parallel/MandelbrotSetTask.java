734d013be5474fe89a50f301c036c07a4c467ef8
Artuhovich Vladislav 350502 <artvlad96@gmail.com>
2015-09-07 23:39:28 +0300

    Changed color from RED to BLUE and from WHITE to YELLOW

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,12 +351,12 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
+            Color.rgb(0, 40, 0),
+            Color.BLUE,
+            Color.YELLOW,
+            Color.BLUE,
+            Color.rgb(40, 100, 0),
+            Color.BLUE,
             Color.rgb(50, 0, 0)
         };
         
