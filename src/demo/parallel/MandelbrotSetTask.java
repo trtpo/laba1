ff5d6b605e2ad28cb3ebd2beaaae7633c0bf3e36
Brel-Egor <brel.egor@mail.ru>
2015-09-11 21:32:23 +0300

    Shape changed +add new functions(-,/)

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex z = new Complex(1, 5);
         do {
-            c = c.times(c).times(c).plus(comp).times(c).plus(comp);
+            c = c.times(c).times(c).division(z).times(c).minus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
