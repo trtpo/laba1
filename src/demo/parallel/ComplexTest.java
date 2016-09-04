bb0d226d9010acc3b7734691e5100fb0e02c8b80
vladimir <nalivaikovova@gmail.com>
2016-09-04 21:51:22 +0300

    Added JUnit tests

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,23 @@
+package demo.parallel;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Vladimir on 04.09.2016.
+ */
+public class ComplexTest extends TestCase {
+    private Complex number;
+    @Before
+    public void setUp() throws Exception {
+        number = new Complex(1,2);
+    }
+
+    @Test
+    public void minus() throws Exception {
+        number.minus(new Complex(2,3));
+        assertEquals(-1,number.getRe(),0.0001);
+        assertEquals(-1,number.getIm(),0.0001);
+    }
+}
