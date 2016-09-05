7b5b6d0252c748dabd56cc809d0fac460e79a430
Dima Volynets 450501 <dimanvolina@gmail.com>
2016-09-06 05:03:23 +0600

    added tests

---
+++ src/test/demo/parallel/ComplexTest.java
@@ -0,0 +1,37 @@
+package demo.parallel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 06.09.2016.
+ */
+public class ComplexTest {
+    private Complex a;
+    private Complex b;
+    private Complex c;
+
+    @Before
+    public void setUp() throws Exception {
+        a = new Complex(1,0);
+        b = new Complex(0,1);
+        c = new Complex(1,1);
+    }
+
+    @Test
+    public void notEqual() throws Exception{
+        assertEquals(" not equals ", c, a.plus(b));
+    }
+
+    @Test
+    public void notNullRe() throws Exception {
+        assertNotEquals(0.0, a.getRe());
+    }
+
+    @Test
+    public void  notNullIm() throws Exception {
+        assertNotEquals(0.0, a.getIm());
+    }
+}
