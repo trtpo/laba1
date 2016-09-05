88b1719e1a7c7fdcee63831aee1981146b5d12fc
Alexei Zablotsky <zork2020@mail.ru>
2016-09-05 11:39:53 +0300

    Added new functions

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,6 +70,11 @@ public class Complex {
         im += b.im;
         return this;
     }
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
 
     /**
      * Multiply operation.
@@ -80,6 +85,14 @@ public class Complex {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
         double imag = a.re * b.im + a.im * b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re*b.re+b.im*b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(b.re*b.re+b.im*b.im);
         re = real;
         im = imag;
         return this;
