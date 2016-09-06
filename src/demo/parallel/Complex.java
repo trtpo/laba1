6f8d09b6ddf6c56c6c604299bd7095fd369693f2
vicctorb2 <vicctor.b.0712@gmail.com>
2016-09-06 16:47:44 +0300

    Changes (d) by Victor Buryak

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -91,6 +91,14 @@ public class Complex {
         return this;
     }
 
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
