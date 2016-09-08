c9476c083c156a82f04d26fa0a07bac615ce2ff1
lexvenim <lexvenim@gmail.com>
2016-09-08 19:06:35 +0300

    Missed division formula fixed

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -103,6 +103,8 @@ public class Complex {
      */
     public Complex divide(Complex b) {
         Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re * b.re + b.im * b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(b.re * b.re + b.im * b.im);
         re = real;
         im = imag;
         return this;
