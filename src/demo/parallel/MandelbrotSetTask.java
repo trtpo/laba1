f875705f3ff1069f7bfb86e7ca0b78ee0c6e19b1
novik-andrey <bignovik@gmail.com>
2016-09-09 10:38:08 +0300

    450503_Novik

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -39,8 +39,7 @@ import javafx.scene.paint.Color;
 
 
 /**
- * Task to render Mandelbrot set using given parameters. See {@link 
- * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
+ * Task to render Mandelbrot set using given parameters. See {@link
  * double, double, double, double, double, double, double, double, boolean) 
  * constructor} for parameters list. The task returns time in milliseconds as 
  * its calculated value.
@@ -275,7 +274,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).times(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -352,11 +351,11 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.YELLOW,
             Color.WHITE,
-            Color.RED,
+            Color.YELLOW,
             Color.rgb(100, 0, 0),
-            Color.RED,
+            Color.YELLOW,
             Color.rgb(50, 0, 0)
         };
         
