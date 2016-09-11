8c0ce1e95f00eca77bf5da448c2cfb0c4eb3e67e
Maksim Verbilo <mistermaxblr@gmail.com>
2016-09-11 23:41:34 +0300

    Subtration and dividing added

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).times(comp).plus(comp).times(comp);
+            c = c.times(c).sub(comp).divide(comp).sub(comp).divide(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -359,13 +359,13 @@ class MandelbrotSetTask extends Task<Long> {
             Color.VIOLET,
             Color.rgb(50, 0, 0)
         };
-        
+
         /**
          * Color stops for colors table: relative position in the table
          */
         double[] cp = {
             0, 0.17, 0.25, 0.30, 0.5, 0.75, 1,};
-        
+
         /**
          * Color table population
          */
