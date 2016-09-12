a25618a1401d2f0949e52cec2e900ccf333e834d
Dima Hudovec <hudovecd@gmail.com>
2016-09-12 13:55:12 +0300

    Test

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -45,7 +45,23 @@ package demo.parallel;
  * @author Alexander Kouznetsov, Tristan Yan
  */
 public class Complex {
-    
+
+    public double getRe() {
+        return re;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setIm(double im) {
+        this.im = im;
+    }
+
     private double re;   // the real part
     private double im;   // the imaginary part
 
