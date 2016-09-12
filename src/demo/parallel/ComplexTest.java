6d8487f22511f5c68741ad8f8faebc2f72f394f7
VadimRataiko450501 <logicno@mail.ru>
2016-09-12 21:12:42 +0300

    Complete task 4

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -26,9 +26,9 @@ public class ComplexTest {
 
     public void setUp() throws Exception {
         System.out.println("Setuping tests...");
-        this.a = new Complex(1, 2);
-        this.b = new Complex(15, 10);
-        this.c = new Complex(3, 6);
+        this.a = new Complex(7, 9);
+        this.b = new Complex(5, 4);
+        this.c = new Complex(1, 8);
         this.d = new Complex(0, 0);
         this.result = new Complex(0, 0);
 
@@ -49,8 +49,8 @@ public class ComplexTest {
     public void plus() throws Exception {
         System.out.println("Perform Plus");
         result = a.plus(b);
-        assertEquals(16, result.getRe(), 0);
-        assertEquals(12, result.getIm(), 0);
+        assertEquals(12, result.getRe(), 0);
+        assertEquals(13, result.getIm(), 0);
     }
 
 
@@ -58,8 +58,8 @@ public class ComplexTest {
     public void plusNull() throws Exception {
         System.out.print("Perform Plus Null");
         result.plus(b);
-        assertEquals(15, result.getRe(), 0);
-        assertEquals(10, result.getIm(), 0);
+        assertEquals(5, result.getRe(), 0);
+        assertEquals(4, result.getIm(), 0);
     }
 
 
@@ -68,8 +68,8 @@ public class ComplexTest {
     public void minus() throws Exception {
         System.out.println("Perform Minus");
         result = a.minus(b);
-        assertEquals(-14, result.getRe(), 0);
-        assertEquals(-8, result.getIm(), 0);
+        assertEquals(2, result.getRe(), 0);
+        assertEquals(5, result.getIm(), 0);
     }
 
 
@@ -77,8 +77,8 @@ public class ComplexTest {
     public void minusNull() throws Exception {
         System.out.println("Perform Minus Null");
         result = a.minus(d);
-        assertEquals(1, result.getRe(), 0);
-        assertEquals(2, result.getIm(), 0);
+        assertEquals(7, result.getRe(), 0);
+        assertEquals(9, result.getIm(), 0);
     }
 
 
@@ -87,8 +87,8 @@ public class ComplexTest {
     public void divRight() throws Exception {
         System.out.println("Perform Division");
         result = b.div(c);
-        assertEquals((double)7/3, result.getRe(), 0.0001);
-        assertEquals((double)-4/3, result.getIm(), 0.0001);
+        assertEquals((double)5, result.getRe(), 0.0001);
+        assertEquals((double)4/8, result.getIm(), 0.0001);
     }
 
 
