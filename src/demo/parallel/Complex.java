bef98caf23091932083798403a39e4fd792a25d1
Vlad Osipov <osipov.06@mail.ru>
2016-09-13 15:41:50 +0300

    New operations. Changed equation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,12 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b){
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
@@ -80,6 +86,15 @@ public class Complex {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
         double imag = a.re * b.im + a.im * b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im);
+        double imag = (b.re * a.im - b.im * a.re) / (b.re * b.re + b.im * b.im);
         re = real;
         im = imag;
         return this;
