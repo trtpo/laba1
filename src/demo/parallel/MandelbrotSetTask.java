6a19a958b8145e54af220d16e443f2ac2d74637d
AndreySychik <andrey.sy4ik@mail.ru>
2016-09-23 00:17:25 +0300

    New function for factorial

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex d = new Complex(-4, 2);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).plus(c).div(comp).div(comp).sub(d);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
