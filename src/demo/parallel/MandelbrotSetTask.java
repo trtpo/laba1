afebd0ebd24452b6f98f692f80674610f947fee9
Ilia Tracevski 350531 <Deeo-sama@hotmail.com>
2016-10-25 00:21:08 +0300

    Colors
    Colors changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.DARKGRAY;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.DARKRED,
+            Color.ORANGE,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.LIGHTBLUE,
+            Color.DARKBLUE,
+            Color.VIOLET
         };
         
         /**
