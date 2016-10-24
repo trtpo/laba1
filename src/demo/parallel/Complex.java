56374bdf4608f174fcb35f5ff798434e1306dcfc
DmitryLenko <dias1381254@gmail.com>
2016-10-24 18:01:00 +0300

    add new function

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,12 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
