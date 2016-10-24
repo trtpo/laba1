d3af39e4dc7a51e7b2704e077854a90a05209962
paulpol <paul.plnkv@gmail.com>
2016-10-24 19:54:13 +0300

    Changed multiply to modulus fractal

--- src/demo/parallel/Tests.java
+++ src/demo/parallel/Tests.java
@@ -12,14 +12,14 @@ public class Tests {
 
     @Before
     public void setUp() throws Exception {
-        number = new Complex(4, 3);
+        number = new Complex(4, 6);
     }
 
     @Test
-    public void multiply() throws Exception {
-        number.multiply(new Complex(3, 5));
-        assertEquals(12, number.getRe(), 1e-15);
-        assertEquals(15, number.getIm(), 1e-15);
+    public void modulus() throws Exception {
+        number.modulus(new Complex(3, 5));
+        assertEquals(5, number.getRe(), 1e-15);
+        assertEquals(8, number.getIm(), 1e-15);
     }
 
 }
