c66033f06dc5c78bf7817ab6a9ffc589dd073050
a.piskaryov <a.piskaryov@itransition.com>
2016-11-04 15:09:00 +0300

    added unit tests

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,3 +1,40 @@
+import demo.parallel.Complex;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UnitTest {
+    private Complex number;
+
+    @Before
+    public void before() throws Exception {
+        number = new Complex(5, 8);
+    }
+
+    @Test
+    public void minusTest() throws Exception {
+        number.minus(new Complex(1, 2));
+        assertEquals(4, number.getRe(), 1e-15);
+        assertEquals(6, number.getIm(), 1e-15);
+    }
+    @Test
+    public void plusTest() throws Exception {
+        number.plus(new Complex(1, 2));
+        assertEquals(6, number.getRe(), 1e-15);
+        assertEquals(10, number.getIm(), 1e-15);
+    }
+    @Test
+    public void multiplyTest() throws Exception {
+        number.multiply(new Complex(1, 2));
+        assertEquals(5, number.getRe(), 1e-15);
+        assertEquals(16, number.getIm(), 1e-15);
+    }
+    @Test
+    public void multipleOperatonTest() throws Exception {
+        number.plus(new Complex(1, 2)).minus(new Complex(2,3));
+        assertEquals(4, number.getRe(), 1e-15);
+        assertEquals(7, number.getIm(), 1e-15);
+    }
 }
+
