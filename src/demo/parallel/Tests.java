b9a84b641e5478d5b03a538f699ddb472b3922f2
paulpol <paul.plnkv@gmail.com>
2016-11-14 17:08:28 +0300

    Add tests for minus and plus

--- src/demo/parallel/Tests.java
+++ src/demo/parallel/Tests.java
@@ -17,9 +17,22 @@ public class Tests {
 
     @Test
     public void modulus() throws Exception {
-        number.modulus(new Complex(3, 5));
-        assertEquals(5, number.getRe(), 1e-15);
-        assertEquals(8, number.getIm(), 1e-15);
+        double modulus = number.modulus();
+        double rightValue = Math.sqrt(Math.pow(number.getRe(), 2) + Math.pow(number.getIm(), 2));
+        assertEquals("Not equal to correct value", rightValue, modulus, 1e-15);
+    }
+
+    @Test
+    public void minus() throws Exception {
+        number.minus(new Complex(3, 5));
+        assertEquals("Should be equals to 1", 1, number.getRe(), 1e-15);
+        assertEquals("Should be equals to 1", 1, number.getIm(), 1e-15);
+    }
+
+    @Test
+    public void plus() throws Exception {
+        number.plus(new Complex(3, 5));
+        assertEquals("Should be equals to 7", 7, number.getRe(), 1e-15);
     }
 
 }
