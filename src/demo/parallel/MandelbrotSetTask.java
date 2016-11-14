f3b36b603b0321ab2dbe8889c36edda6c9adbbbb
DmitryLenko <dias1381254@gmail.com>
2016-11-14 17:26:02 +0300

    deleted multiply func

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -276,7 +276,6 @@ class MandelbrotSetTask extends Task<Long> {
         Complex c = new Complex(0, 0);
         do {
             c = c.times(c).plus(comp);
-            c = c.times(c).plus(comp).multiply(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
