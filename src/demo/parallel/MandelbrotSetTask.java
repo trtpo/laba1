b97d8bb08bd6a459d2aea260e2ac246df1f0a8af
Kolya Grigoruk 550501 <testforname@mail.ru>
2017-09-02 18:12:25 +0300

    added operation

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).times(c).plus(comp).plus(comp);
+            c = c.times(c).times(c).substr(comp).times(c).plus(comp).times(c).substr(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
