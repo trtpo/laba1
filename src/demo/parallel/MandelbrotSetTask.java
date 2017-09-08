cfdc2baad08f7942d99b38aa474e931b90a21cc3
beckon111 <beckon.rus@gmail.com>
2017-09-08 12:05:55 +0300

    color exchange

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(32, 11, 40),
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.WHITE,
+            Color.rgb(20, 33, 100),
+            Color.GRAY,
+            Color.rgb(50, 1, 42)
         };
         
         /**
