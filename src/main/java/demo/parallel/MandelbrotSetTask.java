a22b893846643d59441753948269bad301802a1e
ilya-desktop <com.ilya.ivanov@gmail.com>
2017-09-10 14:40:41 +0300

    Madnelbrot set's color spectrum changed to blue/darkgray.

--- src/main/java/demo/parallel/MandelbrotSetTask.java
+++ src/main/java/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.DARKGRAY;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(8, 0, 40),
+            Color.BLUE,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.BLUE,
+            Color.rgb(20, 0, 100),
+            Color.BLUE,
+            Color.rgb(10, 0, 50)
         };
         
         /**
