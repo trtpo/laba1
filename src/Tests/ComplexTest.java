cabf5201b3a141bba2056d715a4fafdfc8931866
mac <bsuir.dmitriy.dronov@gmail.com>
2017-09-10 14:25:07 +0300

    Added 2 tests for minus operation of Complex class.

---
+++ src/Tests/ComplexTest.java
@@ -0,0 +1,77 @@
+package Tests;
+
+import demo.parallel.Complex;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class ComplexTest extends Assert {
+
+    private HashMap<String, Double> testValues = new HashMap<>();
+
+    @Before
+    public void initTestValues(){
+        Random val = new Random(1000);
+        testValues.put("real1", val.nextDouble());
+        testValues.put("real2", val.nextDouble());
+        testValues.put("image1", val.nextDouble());
+        testValues.put("image2", val.nextDouble());
+    }
+
+    @Test
+    public void getReal() throws Exception {
+    }
+
+    @Test
+    public void getImage() throws Exception {
+    }
+
+    @Test
+    public void negative() throws Exception {
+    }
+
+    @Test
+    public void minus() throws Exception {
+        Complex number1 = new Complex(testValues.get("real1"), testValues.get("image1"));
+        Complex number2 = new Complex(testValues.get("real2"), testValues.get("image2"));
+        number1.minus(number2);
+        assertEquals(testValues.get("real1") - testValues.get("real2"), number1.getReal(), 0.0001);
+        assertEquals(testValues.get("image1") - testValues.get("image2"), number1.getImage(), 0.0001);
+    }
+
+    @Test
+    public void minusItSelf() throws Exception {
+        Complex number1 = new Complex(testValues.get("real1"), testValues.get("image1"));
+        number1.minus(number1);
+        assertEquals(0.0, number1.getReal(), 0.0001);
+        assertEquals(0.0, number1.getImage(), 0.0001);
+    }
+
+    @Test
+    public void divide() throws Exception {
+    }
+
+    @Test
+    public void exp() throws Exception {
+    }
+
+    @Test
+    public void sin() throws Exception {
+    }
+
+    @Test
+    public void cos() throws Exception {
+    }
+
+    @Test
+    public void tg() throws Exception {
+    }
+
+    @Test
+    public void ctg() throws Exception {
+    }
+
+}
