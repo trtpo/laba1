00bad85823e2dfc5a192bd7e4d15b6d70d3eb0f5
Yury Demidok <yury_dem@mail.ru>
2017-09-10 21:56:25 +0300

    Minus and conjugate operations was added. Fractal equation was changed to yc(z) = z^2 + c + (c - z)^(overline)

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -72,6 +72,17 @@ public class Complex {
     }
 
     /**
+     * Subtract operation.
+     * @param b  subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
@@ -82,6 +93,15 @@ public class Complex {
         double imag = a.re * b.im + a.im * b.re;
         re = real;
         im = imag;
+        return this;
+    }
+
+    /**
+     * Conjugate finding operation
+     * @return this Complex object whose value is conjugate
+     */
+    public Complex conjugate() {
+        im = -im;
         return this;
     }
 
