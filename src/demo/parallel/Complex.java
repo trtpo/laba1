90d1a074ea4ede4f9581be06135ca33e5dfb4c81
Olga <vol.bondarava@gmail.com>
2017-09-11 01:21:46 +0300

    Added 2 operations on complex numbers (grade and minus). Changed fractal equation.

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,33 @@ public class Complex {
     }
 
     /**
+     * Minus operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re - b.re;
+        double imag = a.im - b.im;
+        re = real;
+        im = imag;
+        return  this;
+    }
+
+    /**
+     * Grade operation.
+     * @param  b grade
+     * @return this Complex object whose value is this ^ b
+     */
+    public Complex grade(int b) {
+        Complex a = this;
+        Complex step = this;
+        while (b-- > 1)
+            a = a.times(step);
+        return a;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).times(c);
+            c = c.grade(2).plus(comp).minus(new Complex(0.2, -0.2));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
