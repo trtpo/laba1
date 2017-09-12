55948b61e862fe591c96b762c6a4b21f4bf17111
AJIOB <aleksandr9809@gmail.com>
2017-09-12 23:20:10 +0300

    Fractal z[n+1] = cos(z[n])/c

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.cos().plus(new Complex(1,0).divide(comp));
+            c = c.cos().divide(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
