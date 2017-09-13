46208b3bd77cf8fd00e645926b29e30050cac310
Алексей <Fyodorov.aleksej@gmail.com>
2017-09-13 14:38:54 +0300

    Editing colors of fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(230, 100, 50),
+            Color.YELLOW,
+            Color.GREEN,
+            Color.BLUE,
+            Color.rgb(100, 20, 200),
+            Color.BLUE,
+            Color.rgb(255, 255, 100)
         };
         
         /**
