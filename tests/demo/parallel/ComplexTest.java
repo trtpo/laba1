fe4a06bc0ea07813f3b2acc1b74b85804dd7fced
Nikolai <nikolaikolyago@gmail.com>
2017-09-13 19:17:31 +0300

    Reformat some code to match screen width
    Signed-off-by: Nikolai <nikolaikolyago@gmail.com>

--- tests/demo/parallel/ComplexTest.java
+++ tests/demo/parallel/ComplexTest.java
@@ -19,15 +19,20 @@ class ComplexTest {
     @Test
     void divide() {
         Complex temp = z1.divide(z2);
-        assertEquals(temp.getRe(), -41.0 / 173.0, "First pair result real part has wrong value (in Complex.divide(Complex))");
-        assertEquals(temp.getIm(), -7.0 / 173.0, "First pair result imaginary part has wrong value (in Complex.divide(Complex))");
-        assertThrows(ArithmeticException.class, () -> z1.divide(new Complex(0, 0)), "Division by zero in Complex.divide(Complex)");
+        assertEquals(temp.getRe(), -41.0 / 173.0,
+                "Real part has wrong value (in Complex.divide(Complex))");
+        assertEquals(temp.getIm(), -7.0 / 173.0,
+                "Imaginary part has wrong value (in Complex.divide(Complex))");
+        assertThrows(ArithmeticException.class, () -> z1.divide(new Complex(0, 0)),
+                "Division by zero in Complex.divide(Complex)");
     }
 
     @Test
     void minus() {
         Complex temp = z1.minus(z2);
-        assertEquals(temp.getRe(), -3.0, "First pair result real part has wrong value (in Complex.minus(Complex))");
-        assertEquals(temp.getIm(), 16.0, "First pair result imaginary part has wrong value (in Complex.minus(Complex))");
+        assertEquals(temp.getRe(), -3.0,
+                "Real part has wrong value (in Complex.minus(Complex))");
+        assertEquals(temp.getIm(), 16.0,
+                "Imaginary part has wrong value (in Complex.minus(Complex))");
     }
 }
