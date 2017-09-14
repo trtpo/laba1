b5388dd589c03df1b8ae09726c817bb6dcf72c14
gnFur <patych7@gmail.com>
2017-09-14 23:47:36 +0300

    Изменил уравнение фрактала.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex o = new Complex(0.5, 0.5);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(c).plus(comp).plus(o);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
