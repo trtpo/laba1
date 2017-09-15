851fca36e447948e30349065093ec0f5dfde33d7
Andrei Matusevich <matusievic@gmail.com>
2017-09-15 21:23:32 +0300

    Test class was updated
    @BeforeAll annotalion was added

--- test/demo/parallel/ComplexTest.java
+++ test/demo/parallel/ComplexTest.java
@@ -1,45 +1,58 @@
 package demo.parallel;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComplexTest {
+    //objects for division test
+    private static Complex numeratorForDivisionTest;
+    private static Complex nonZeroDenominatorForDivisionTest;
+    private static Complex zeroDenominatorForDivisionTest;
+    //objects for sine function test
+    private static Complex zeroNumberForSineTest;
+    private static Complex nonZeroNumberForSineTest;
+    //objects for cosine function test
+    private static Complex zeroNumberForCosineTest;
+    private static Complex nonZeroNumberForCosineTest;
+
+    @BeforeAll
+    static void initializeTest() {
+        numeratorForDivisionTest = new Complex(-2, 1);
+        nonZeroDenominatorForDivisionTest = new Complex(1, -1);
+        zeroDenominatorForDivisionTest = new Complex(0, 0);
+        zeroNumberForSineTest = new Complex(0, 0);
+        nonZeroNumberForSineTest = new Complex(3, 5);
+        zeroNumberForCosineTest = new Complex(0, 0);
+        nonZeroNumberForCosineTest = new Complex(5, -5.32);
+    }
+
     @Test
     void divideOperationTest() {
-        Complex Numerator = new Complex(-2, 1);
-        Complex nonZeroDenominator = new Complex(1, -1);
         Complex expectedResult = new Complex(-1.5, -0.5);
-        Complex zeroDenominator = new Complex(0, 0);
-
         assertAll("Divide operation test",
-                () -> assertEquals(Numerator.div(nonZeroDenominator), expectedResult, "Divide by non-zero test"),
-                () -> assertThrows(ArithmeticException.class, () -> Numerator.div(zeroDenominator), "Divide by zero test"));
+                () -> assertEquals(numeratorForDivisionTest.div(nonZeroDenominatorForDivisionTest), expectedResult, "Divide by non-zero test"),
+                () -> assertThrows(ArithmeticException.class, () -> numeratorForDivisionTest.div(zeroDenominatorForDivisionTest), "Divide by zero test"));
     }
 
     @Test
     void sineFunctionTest() {
-        Complex zeroNumber = new Complex(0, 0);
         Complex expectedSineOfZero = new Complex(0, 0);
-
-        Complex nonZeroNumber = new Complex(3, 5);
         Complex expectedSizeofNonZero = new Complex(10.47, -73.46);
 
         assertAll("Sine function test",
-                () -> assertEquals(zeroNumber.sin(), expectedSineOfZero, "Sine function test for zero"),
-                () -> assertEquals(nonZeroNumber.sin(), expectedSizeofNonZero, "Sine function test for non-zero"));
+                () -> assertEquals(zeroNumberForSineTest.sin(), expectedSineOfZero, "Sine function test for zero"),
+                () -> assertEquals(nonZeroNumberForSineTest.sin(), expectedSizeofNonZero, "Sine function test for non-zero"));
     }
 
     @Test
     void cosineFunctionTest() {
-        Complex zeroNumber = new Complex(0, 0);
         Complex expectedCosineOfZero = new Complex(1, 0);
-
-        Complex nonZeroNumber = new Complex(5, -5.32);
         Complex expectedCosineOfNonZero = new Complex(28.99, -97.99);
 
         assertAll("Cosine function test",
-                () -> assertEquals(zeroNumber.cos(), expectedCosineOfZero, "Cosine function test for zero"),
-                () -> assertEquals(nonZeroNumber.cos(), expectedCosineOfNonZero, "Cosine function test for non-zero"));
+                () -> assertEquals(zeroNumberForCosineTest.cos(), expectedCosineOfZero, "Cosine function test for zero"),
+                () -> assertEquals(nonZeroNumberForCosineTest.cos(), expectedCosineOfNonZero, "Cosine function test for non-zero"));
     }
 
 }
