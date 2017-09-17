c1a74e76f92909756ee2c24863ad792823651ee0
KseniyaShchemeliova <kseniya.shchamialiova@gmail.com>
2017-09-17 18:10:21 +0300

    change color

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(91, 190, 155),
+            Color.PURPLE,
+            Color.YELLOW,
+            Color.GOLD,
+            Color.rgb(138, 44, 148),
+            Color.BLUE,
+            Color.rgb(95, 90, 168)
         };
         
         /**
