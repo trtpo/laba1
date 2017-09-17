7e34e06ec3f8d529a5ea78d06ab2d59fbfb88e60
AntonMelekhavets550504 <antohage@gmail.com>
2017-09-17 14:24:22 +0300

    Changing the colors of a fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.DARKORANGE;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
+            Color.rgb(100, 20, 50),
+            Color.YELLOW,
             Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.YELLOW,
+            Color.rgb(80, 0, 100),
+            Color.YELLOW,
+            Color.rgb(50, 80, 80)
         };
         
         /**
