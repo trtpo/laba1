2bb2e652a833f09ccdbfbd69a0db8a885dff7fac
HaidukYury <HaidukYury@gmail.com>
2017-09-17 16:38:36 +0300

    Changed Color Scheme to green-violet

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(40, 90, 50),
+            Color.VIOLET,
             Color.WHITE,
-            Color.RED,
+            Color.VIOLET,
             Color.rgb(140, 90, 140),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.VIOLET,
+            Color.rgb(40, 90, 50)
         };
         
         /**
