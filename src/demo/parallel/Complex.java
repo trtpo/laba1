a74db9623e652c1c920689420a7b7dc577a8daf2
KseniyaShchemeliova <kseniya.shchamialiova@gmail.com>
2017-09-17 18:34:40 +0300

    add function

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,17 @@ public class Complex {
     }
 
     /**
+     * subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex minus(Complex b){
+        re-=b.re;
+        im+=b.im;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
