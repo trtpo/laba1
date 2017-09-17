4f261d101dd6559ed0cb9e1ab6d144681705d41f
Nikita Kozelko <nikitakozelko@gmail.com>
2017-09-17 19:08:43 +0300

    Add new operation(subtraction, division) for complex numbers. Use new operation in expression fractal.

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,31 @@ public class Complex {
     }
 
     /**
+     * Substruction operation.
+     * @param  b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex substruct(Complex b) {
+        re -= b.re;
+        im -= b.re;
+        return this;
+    }
+
+    /**
+     * Division operation.
+     * @param  b divisor
+     * @return this Complex object whose value is this / b
+     */
+    public Complex division(Complex b) {
+        Complex a = this;
+        double real = (a.re * a.im + b.re * b.im)/(a.im * a.im + b.im * b.im);
+        double imag = (a.im * b.re -a.re * b.im)/(a.im * a.im + b.im * b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
