fb7c8a1a908e56ef5f7736703783170dd982f03c
Vladislav Kovalev <vladislav.kovalev.alekseevich@gmail.com>
2017-09-18 00:21:05 +0300

    Add function

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,20 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+        }
+
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re*b.re + a.im*b.im) / (b.re*b.re + b.im*b.im);
+        double imq = (b.re*a.im - a.re*b.im) / (b.re*b.re + b.im*b.im);
+        re = real;
+        im = imq;
+        return this;
+        }
     /**
      * Multiply operation.
      * @param  b multiplier
