9833655d6aeb1b4a2d50bba52058733e63f4afb5
v.tarasevich <tarasevich.vlad.97@gmail.com>
2017-09-17 21:42:01 +0300

    Added equals and hashCode methods (Override)

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -106,6 +106,28 @@ public class Complex {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Complex complex = (Complex) o;
+
+        if (Double.compare(complex.re, re) != 0) return false;
+        return Double.compare(complex.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(re);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(im);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
     /**
      * Square of Complex object's length, we're using square of length to
      * eliminate the computation of square root
