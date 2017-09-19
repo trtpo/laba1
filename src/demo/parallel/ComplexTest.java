709ff966b38832a0bba6e7b2c345ed9e15346d01
mldalos <joker.leha98@gmail.com>
2017-09-19 10:13:07 +0300

    Add information in tests

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -10,13 +10,16 @@ public class ComplexTest {
     @org.junit.Test
     public void minus_positive() throws Exception {
         Complex num = new Complex(23,18).minus(new Complex(20,2));
-        assertTrue(new Complex(3,16).equals(num));
+        assertEquals("Wrong real part in method minus(object: Complex(23, 18), argument: Complex(20, 2)) of class Complex(file: demo.parallel.Complex.java)", 3, num.getRe(),0);
+        assertEquals("Wrong imagine part in method minus(object: Complex(23, 18), argument: Complex(20, 2)) of class Complex(file: demo.parallel.Complex.java)", 16, num.getIm(),0);
     }
 
     @org.junit.Test
     public void minus_negative() throws Exception {
         Complex num = new Complex(3,8).minus(new Complex(8,15));
-        assertTrue(new Complex(-5,-7).equals(num));
+        assertEquals("Wrong real part in method minus(object: Complex(3, 8), argument: Complex(8, 15)) of class Complex(file: demo.parallel.Complex.java)", -5, num.getRe(),0);
+        assertEquals("Wrong imagine part in method minus(object: Complex(3, 8), argument: Complex(8, 15)) of class Complex(file: demo.parallel.Complex.java)", -7, num.getIm(),0);
+        
     }
 
 }
