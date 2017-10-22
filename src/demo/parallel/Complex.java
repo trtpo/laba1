ede018428f6137781f853a37fb0a9ee41d78d132
Anya <anna.zazybo@mail.ru>
2017-10-23 02:36:02 +0400

    c. Добавление операций деления и вычитания и их использование.

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,6 +70,27 @@ public class Complex {
         im += b.im;
         return this;
     }
+    
+    public Complex minus(Complex b) {
+    	re -= b.re;
+    	im -= b.im;
+    	return this;
+    }
+    
+    /**
+    * Divide operation.
+    * @param  b divisor
+    * @return this Complex object whose value is this / b
+    */
+    public Complex divide(Complex b) {
+    	Complex a = this;
+    	if ((b.re * b.re + b.im * b.im)==0) throw new java.lang.ArithmeticException("Nel'zja delit' na 0");
+    	double real = (a.re * b.re + a.im * b.im)/(b.re * b.re + b.im * b.im);
+    	double imag = (b.re * a.im - a.re * b.im)/(b.re * b.re + b.im * b.im);
+    	re = real;
+    	im = imag;
+    	return this;
+    }
 
     /**
      * Multiply operation.
