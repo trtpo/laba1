7a875d2d33e2a980d7d72d7897e45b5c70e0775f
Роман Юдо 550502 <32337133+ChudoYudo@users.noreply.github.com>
2017-12-20 17:51:08 +0300

    Add new complex function

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -30,67 +30,87 @@
  */
 package demo.parallel;
 
-
 /**
- * A complex number is a number that can be expressed in the form a + b * i, where
- * a and b are real numbers and i is the imaginary unit, which satisfies the
- * equation i ^ 2 = -1. a is the real part and b is the imaginary part of the
- * complex number.
- * <p><i>
- * This source code is provided to illustrate the usage of a given feature
- * or technique and has been deliberately simplified. Additional steps
- * required for a production-quality application, such as security checks,
- * input validation and proper error handling, might not be present in
- * this sample code.</i>
+ * A complex number is a number that can be expressed in the form a + b * i,
+ * where a and b are real numbers and i is the imaginary unit, which satisfies
+ * the equation i ^ 2 = -1. a is the real part and b is the imaginary part of
+ * the complex number.
+ * <p>
+ * <i> This source code is provided to illustrate the usage of a given feature
+ * or technique and has been deliberately simplified. Additional steps required
+ * for a production-quality application, such as security checks, input
+ * validation and proper error handling, might not be present in this sample
+ * code.</i>
+ * 
  * @author Alexander Kouznetsov, Tristan Yan
  */
 public class Complex {
-    
-    private double re;   // the real part
-    private double im;   // the imaginary part
 
-    /** 
-     * create a new object with the given real and imaginary parts
-     * 
-     * @param real a complex number real part
-     * @param imag a complex number imaginary part 
-     */
-    public Complex(double real, double imag) {
-        re = real;
-        im = imag;
-    }
+	private double re; // the real part
+	private double im; // the imaginary part
 
-    /**
-     * Add operation.
-     * @param b summand
-     * @return this Complex object whose value is (this + b)
-     */
-    public Complex plus(Complex b) {
-        re += b.re;
-        im += b.im;
-        return this;
-    }
+	/**
+	 * create a new object with the given real and imaginary parts
+	 * 
+	 * @param real
+	 *            a complex number real part
+	 * @param imag
+	 *            a complex number imaginary part
+	 */
+	public Complex(double real, double imag) {
+		re = real;
+		im = imag;
+	}
 
-    /**
-     * Multiply operation.
-     * @param  b multiplier
-     * @return this Complex object whose value is this * b
-     */
-    public Complex times(Complex b) {
-        Complex a = this;
-        double real = a.re * b.re - a.im * b.im;
-        double imag = a.re * b.im + a.im * b.re;
-        re = real;
-        im = imag;
-        return this;
-    }
+	/**
+	 * Add operation.
+	 * 
+	 * @param b
+	 *            summand
+	 * @return this Complex object whose value is (this + b)
+	 */
+	public Complex plus(Complex b) {
+		re += b.re;
+		im += b.im;
+		return this;
+	}
 
-    /**
-     * Square of Complex object's length, we're using square of length to 
-     * eliminate the computation of square root
-     * @return square of length
-    */
-    public double lengthSQ() {
-        return re * re + im * im;
-    }
+	/**
+	 * Multiply operation.
+	 * 
+	 * @param b
+	 *            multiplier
+	 * @return this Complex object whose value is this * b
+	 */
+	public Complex times(Complex b) {
+		Complex a = this;
+		double real = a.re * b.re - a.im * b.im;
+		double imag = a.re * b.im + a.im * b.re;
+		re = real;
+		im = imag;
+		return this;
+	}
+
+	/**
+	 * Square of Complex object's length, we're using square of length to eliminate
+	 * the computation of square root
+	 * 
+	 * @return square of length
+	 */
+	public double lengthSQ() {
+		return re * re + im * im;
+	}
+
+	/**
+	 * Substraction of complex numbers.
+	 * 
+	 * @param c
+	 * @return
+	 */
+
+	public Complex sub(Complex c) {
+		this.re -= c.re;
+		this.im -= c.re;
+		return this;
+	}
 }
