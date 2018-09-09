5b0bea925c37c1f95c7efe33f7e6834bba683702
desmond1999d <desmond1999d@mail.ru>
2018-09-09 17:29:34 +0300

    Fractal was changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(c).times(c).plus(comp.times(new Complex(-1, 1)));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.BLUE;
         }
         return colors[count];
     }
@@ -353,7 +353,7 @@ class MandelbrotSetTask extends Task<Long> {
         Color[] cc = {
             Color.rgb(40, 0, 0),
             Color.WHITE,
-            Color.BLUEVIOLET,
+            Color.GREEN,
             Color.RED,
             Color.rgb(100, 0, 0),
             Color.RED,
