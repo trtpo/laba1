b4e73628eb175d9527e42bf8cde145f40a784082
rokez98 <vrokez@gmail.com>
2018-09-09 15:40:24 +0300

    Color gamma changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.YELLOW;
         }
         return colors[count];
     }
@@ -352,12 +352,12 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.GREEN,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.rgb(90, 0, 0),
+            Color.GREEN,
+            Color.rgb(60, 0, 0)
         };
         
         /**
