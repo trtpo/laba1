9d8aa3eca924205a903e513c330142c3f46afea3
fedosenk0 <fedosenk0@inbox.ru>
2018-09-11 00:04:48 +0300

    цвет

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.LIGHTSEAGREEN;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.rgb(240, 70, 250),
+                Color.BLUE,
+                Color.WHITE,
+                Color.BLUE,
+                Color.rgb(100, 0, 100),
+                Color.BLUE,
+                Color.rgb(10, 60, 200)
         };
         
         /**
