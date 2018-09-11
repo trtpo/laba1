2ebe7cab43649d1cf0cb9b92a3cfa3855c9c96cc
Nikolay Dik 650505 <baulkynut@gmail.com>
2018-09-11 19:56:32 +0300

    Added Tests for Sub func & infinity tests for both

--- tests/demo/parallel/ComplexTest.java
+++ tests/demo/parallel/ComplexTest.java
@@ -63,6 +63,72 @@ class ComplexTest {
     }
 
     @Test
+    void divINF() {
+        // Arrange
+        Complex a = new Complex(3,Double.POSITIVE_INFINITY);
+        Complex b = new Complex(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+        // Act
+        Complex result = a.div(b);
+
+        // Assert
+        assertEquals(Double.NaN, result.getReal(), 0.1);
+        assertEquals(Double.NaN, result.getImag(), 0.1);
+    }
+
+    @Test
     void sub() {
+        // Arrange
+        Complex a = new Complex(2,4);
+        Complex b = new Complex(1,1);
+
+        // Act
+        Complex result = a.sub(b);
+
+        // Assert
+        assertEquals(1.0, result.getReal(), 0.1);
+        assertEquals(3.0, result.getImag(), 0.1);
+    }
+
+    @Test
+    void subNeg() {
+        // Arrange
+        Complex a = new Complex(2,-4);
+        Complex b = new Complex(-1,-3);
+
+        // Act
+        Complex result = a.sub(b);
+
+        // Assert
+        assertEquals(3.0, result.getReal(), 0.1);
+        assertEquals(-1.0, result.getImag(), 0.1);
+    }
+
+    @Test
+    void subZero() {
+        // Arrange
+        Complex a = new Complex(2,0);
+        Complex b = new Complex(0,1);
+
+        // Act
+        Complex result = a.sub(b);
+
+        // Assert
+        assertEquals(2.0, result.getReal(), 0.1);
+        assertEquals(-1.0, result.getImag(), 0.1);
+    }
+
+    @Test
+    void subINF() {
+        // Arrange
+        Complex a = new Complex(2,Double.POSITIVE_INFINITY);
+        Complex b = new Complex(Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
+
+        // Act
+        Complex result = a.sub(b);
+
+        // Assert
+        assertEquals(Double.NEGATIVE_INFINITY, result.getReal(), 0.1);
+        assertEquals(Double.NaN, result.getImag(), 0.1);
     }
 }
