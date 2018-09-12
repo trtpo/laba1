d434c72064a511dcceec72117298386f3f988dfa
Nikita <nikita.stets.999@gmail.com>
2018-09-13 00:03:02 +0300

    Division and substraction operation were added to Complex Class

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,35 @@ public class Complex {
     }
 
     /**
+     * Multiply operation.
+     * @param  b multiplier
+     * @return this Complex object whose value is this / b
+     */
+    public Complex division(Complex b) {
+        Complex a = this;
+        double real =(a.re*b.re + a.im*b.im)/(b.re*b.re+b.im*b.im);
+        double imag =(a.im*b.re -a.re*b.im)/(b.re*b.re+b.im*b.im);
+        re = real;
+        im = imag;
+
+        return this;
+    }
+
+    /**
+     * Multiply operation.
+     * @param  b multiplier
+     * @return this Complex object whose value is this - b
+     */
+    public Complex substraction(Complex b) {
+        Complex a = this;
+        double real =a.re-b.re;
+        double imag =a.im-b.im;
+        re = real;
+        im = imag;
+
+        return this;
+    }
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
