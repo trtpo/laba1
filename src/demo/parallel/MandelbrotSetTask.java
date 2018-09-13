4b9bf9017f1d36206e7b98dd7722dbf4ab37e380
Vanya112 <parhamovic@mail.ru>
2018-09-13 14:08:12 +0300

    Change function getColor

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -39,11 +39,7 @@ import javafx.scene.paint.Color;
 
 
 /**
- * Task to render Mandelbrot set using given parameters. See {@link 
- * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
- * double, double, double, double, double, double, double, double, boolean) 
- * constructor} for parameters list. The task returns time in milliseconds as 
- * its calculated value.
+
  * 
  * <p><i>
  * This source code is provided to illustrate the usage of a given feature
@@ -335,7 +331,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.GOLD;
         }
         return colors[count];
     }
