b2d72c69bd54f00f45acfcc299db8af5dd723152
Darya Kozukova <DKozukova@gmail.com>
2018-09-13 14:15:16 +0300

    Добавление Unit тестов

---
+++ src/Tests/ComplexTests.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import demo.parallel.Complex;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class ComplexTests {
+    private Complex value1;
+    private Complex value2;
+    private Complex value3;
+
+
+    @Before
+    public void init(){
+        value1 = new Complex(1,1);
+        value2 = new Complex(4,5);
+        value3 = new Complex(0,0);
+
+    }
+
+    @Test
+    public void subComplex(){
+        Complex res = value2.sub(value1);
+        assertEquals(3, res.getRe(), 0);
+        assertEquals(4, res.getIm(), 0);
+        // assertEquals(9, res.getIm(), 0);
+    }
+
+    @Test
+    public void divComplex(){
+        Complex res = value2.div(value1);
+        assertEquals(4.5, res.getRe(), 0);
+        assertEquals(0.5, res.getIm(), 0);
+    }
+
+    @Test
+    public void divComplexNull(){
+        Complex res = value2.div(value3);
+        assertEquals(4, res.getRe(), 0);
+        assertEquals(5, res.getIm(), 0);
+    }
+
+
+}
