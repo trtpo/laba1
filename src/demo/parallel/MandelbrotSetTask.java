b18e09fc162bbb56f78d6ba3694f1148777c5ffc
maks-buren630501 <43178149+maks-buren630501@users.noreply.github.com>
2018-09-13 15:28:59 +0300

    remake color buren630501

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(40, 70, 80),
+            Color.BLUE,
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
+            Color.rgb(100, 100, 90),
             Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(50, 80, 90)
         };
         
         /**
