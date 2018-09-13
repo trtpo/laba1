9fc9bf3c34b827147f494a46371faa9b8f9e1e12
Vanya112 <parhamovic@mail.ru>
2018-09-13 18:28:05 +0300

    upgrade my fractal function

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -271,7 +271,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).plus(c.times(comp)).times(c);
+            c = c.times(c.times(c.times(c).plus(comp)).plus(comp)).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
