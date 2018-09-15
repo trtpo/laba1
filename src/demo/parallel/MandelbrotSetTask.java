fa1e7d94e385251c77dc326f61861ca52547e494
Артём Терешкович <artyomt7777@gmail.com>
2018-09-15 11:36:24 +0300

    add divide and minus complex operation

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).times(c).times(comp);
+            c = c.times(c).times(c).minus(comp).divide(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -351,14 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-
-                Color.GRAY,
-                Color.BLUE,
-                Color.LIGHTSALMON,
-                Color.BURLYWOOD,
-                Color.DIMGREY,
-                Color.AZURE,
-                Color.IVORY
+            Color.GRAY,
+            Color.BLUE,
+            Color.LIGHTSALMON,
+            Color.BURLYWOOD,
+            Color.DIMGREY,
+            Color.AZURE,
+            Color.IVORY
         };
         
         /**
