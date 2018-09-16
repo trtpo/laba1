c32cc0942b1762eefb6cc4cfc38380b4cbc88f6c
VladMigura <vlad_migura@mail.ru>
2018-09-16 15:21:58 +0300

    Change color spectrum to dark gray

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(80, 80, 80),
+            Color.DARKGRAY,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.DARKGRAY,
+            Color.rgb(100, 100, 100),
+            Color.DARKGRAY,
+            Color.rgb(50, 50, 50)
         };
         
         /**
