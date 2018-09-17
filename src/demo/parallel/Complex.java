a5f234f2827afbe4f09de98a624a811cd207882c
VladMigura <vlad_migura@mail.ru>
2018-09-17 11:38:33 +0300

    Create junit tests for new operations

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -136,4 +136,12 @@ public class Complex {
     public Complex cos() {
         return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
     }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
 }
