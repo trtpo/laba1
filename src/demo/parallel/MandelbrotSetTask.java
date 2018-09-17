42f7a9b9702444cde013abc8f4b4af2db1feaefc
AleshaShch <36892162+AleshaShch@users.noreply.github.com>
2018-09-18 01:55:19 +0300

    Add another color gamut
    Signed-off-by: AleshaShch <36892162+AleshaShch@users.noreply.github.com>

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(20, 60, 60),
+            Color.ORCHID,
+            Color.SILVER,
+            Color.AZURE,
+            Color.rgb(10, 15, 25),
+            Color.SNOW,
+            Color.rgb(0, 0, 50)
         };
         
         /**
