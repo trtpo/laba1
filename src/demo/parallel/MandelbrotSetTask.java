cb5ef7b5f3bbfbe159bf6415318207bac69bbe04
Shvingelskiy <g246113@mail.ru>
2018-09-18 02:09:11 +0300

    Сhanged the function of creating a fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,10 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex temp = new Complex(0,0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(temp).times(c).times(temp).plus(comp);
+            temp = temp.times(new Complex(0.1,0.2)).times(temp).plus(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
