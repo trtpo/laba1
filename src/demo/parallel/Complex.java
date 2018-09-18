9dacc793bb1c28606e41f7e59bd57931c05b504a
Maxim Borodin <fanatik.9898@gmail.com>
2018-09-18 17:09:29 +0300

    add tests

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -31,6 +31,8 @@
 package demo.parallel;
 
 
+import java.util.Objects;
+
 /**
  * A complex number is a number that can be expressed in the form a + b * i, where
  * a and b are real numbers and i is the imaginary unit, which satisfies the
@@ -71,6 +73,20 @@ public class Complex {
         re += b.re;
         im += b.im;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Complex complex = (Complex) o;
+        return Double.compare(complex.re, re) == 0 &&
+                Double.compare(complex.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(re, im);
     }
 
     /**
