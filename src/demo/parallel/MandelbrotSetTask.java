bcb6544426f799b5f477760055952184e043bbfc
NikitaUstyushenko <nikita09061999@mail.ru>
2018-09-18 15:49:33 +0300

    change equation with new operations(division and subtraction)

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,8 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).times(new Complex(2, 1.75)).times(comp);
+            c = c.times(c).plus(comp).times(comp)
+                    .division(comp).subtraction(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
