4174f01793668e34bdea879c311fd07a4e172a38
Your Name <alexei.mairyn@gmail.com>
2018-09-18 10:38:43 +0300

    Merge errors

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,28 @@
+package demo.parallel;
+
+        import static org.junit.Assert.*;
+
+public class ComplexTest {
+    private static final double DELTA = 1e-15;
+    @org.junit.Test
+    public void minus() {
+        Complex myNumber = new Complex(24,6);
+        Complex tested = new Complex(12, 3);
+        assertEquals("Check numbers",tested.getReal(), myNumber.minus(tested).getReal(), DELTA);
+    }
+
+    @org.junit.Test
+    public void divide() {
+        Complex a = new Complex(12, -3);
+        Complex b = new Complex(3, 3);
+        a.divide(b);
+        assertTrue("Check numbers",a.equal(new Complex(1.5, -2.5)));
+    }
+    @org.junit.Test
+    public void equal() {
+        Complex a = new Complex(12, -3);
+        Complex b = new Complex(3, 3);
+        assertEquals("Not equal numbers",true,a.equal(a));
+
+    }
+}
