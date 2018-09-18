4f4c0426817d7d8a7759b0a61e8b2023561a49c1
Nikita <nkaleko@gmail.com>
2018-09-18 23:45:56 +0300

    The third test has been added! The structure of the tests has been changed

--- test/tests/ComplexTest.java
+++ test/tests/ComplexTest.java
@@ -9,21 +9,30 @@ class ComplexTest {
 
     @Test
     void multiply() {
-        Complex test = new Complex(10, 10);
+        Complex test1 = new Complex(10, 10);
+        Complex test2 = new Complex(10, 10);
         Complex result = new Complex(100, 100);
-        test = test.plus(test);
-        assert (test == result);
+        test1 = test1.multiply(test2);
+        assert (test1.getIm() == result.getIm());
+        assert (test1.getRe() == result.getRe());
     }
 
     @Test
     void division() {
         Complex test = new Complex(10, 10);
         Complex result = new Complex(1, 1);
-        test = test.plus(test);
-        assert (test == result);
+        test = test.division(test);
+        assert (test.getIm() == result.getIm());
+        assert (test.getRe() == result.getRe());
     }
 
     @Test
     void minus() {
+        Complex test1 = new Complex(120, 110);
+        Complex test2 = new Complex(10, 10);
+        Complex result = new Complex(110, 100);
+        test1 = test1.minus(test2);
+        assert (test1.getIm() == result.getIm());
+        assert (test1.getRe() == result.getRe());
     }
 }
