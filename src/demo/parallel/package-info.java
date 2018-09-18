5e1cad4a02060bb706116126730e8c228daa7555
NikitaUstyushenko <nikita09061999@mail.ru>
2018-09-18 16:31:48 +0300

    added package-info classes

---
+++ src/demo/parallel/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package stores basic classes for application.
+ *
+ * @author Nikita
+ * @since 17.09.2018
+ * @version 1.0
+ * */
+package demo.parallel;
---
+++ src/tests/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package stores test classes.
+ *
+ * @author Nikita
+ * @since 18.09.2018
+ * @version 1.0
+ * */
+package tests;
