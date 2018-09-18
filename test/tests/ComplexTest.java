95d7c91054b92d5053185c4b0a429541143d989c
Nikita <nkaleko@gmail.com>
2018-09-18 23:28:03 +0300

    The first test has been added

---
+++ test/tests/ComplexTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+//import org.junit.jupiter.api.Test;
+import demo.parallel.Complex;
+import org.junit.jupiter.api.*;
+import demo.parallel.Complex.*;
+
+class ComplexTest {
+
+    @Test
+    void multiply() {
+        Complex test = new Complex(10, 10);
+        Complex result = new Complex(100, 100);
+        test = test.plus(test);
+        assert (test == result);
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void minus() {
+    }
+}
