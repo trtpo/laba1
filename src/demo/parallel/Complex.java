e61b8fbfccf3cb5835ab4318877cbfe85960bf1f
IlliaVysotski <illia.vysotski@mail.ru>
2018-09-20 00:55:46 +0300

    added other operations on complex numbers

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,37 @@ public class Complex {
     }
 
     /**
+     * Sin(z) operation.
+     * @return a new Complex object whose value is the complex sine of this
+     */
+    public Complex sin() {
+        return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
+    }
+    /**
+     * Cos(z) operation.
+     * @return a new Complex object whose value is the complex cosine of this
+     */
+    public Complex cos() {
+        return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
+    }
+    /**
+     *
+     * @return a new Complex object whose value is the reciprocal of this
+     */
+    public Complex reciprocal() {
+        double scale = re*re + im*im;
+        return new Complex(re / scale, -im / scale);
+    }
+    /**
+     *
+     * @return a / b
+     */
+    public Complex divides(Complex b) {
+        Complex a = this;
+        return a.times(b.reciprocal());
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
