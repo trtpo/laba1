c81ece8e25467b2312c0251c43c6da07e5f97b89
Antoś Jurevič <yurevich-anton@mail.ru>
2018-09-20 00:18:34 +0300

    Changed Unit tests

--- test/demo/parallel/ComplexTest.java
+++ test/demo/parallel/ComplexTest.java
@@ -1,20 +1,24 @@
-import demo.parallel.Complex;
+package demo.parallel;
 import org.junit.Test;
 
 
 import static org.junit.Assert.assertEquals;
 
-class ComplexTest {
+public class ComplexTest {
 
     @Test
     public void subtraction() {
         Complex result = new Complex(5,5).subtraction(new Complex(1,2));
-        assertEquals(result, new Complex(4,3));
+        assertEquals((long)result.getRe(), 4);
+        assertEquals((long)result.getIm(), 3);
     }
 
     @Test
     public void division() {
         Complex result = new Complex(5,6).division(new Complex(1,1));
-        assertEquals(result, new Complex(5.5,0.5));
+        double Re = 5.5;
+        double Im = 0.5;
+        assertEquals(result.getRe(), Re, 0.0001);
+        assertEquals(result.getIm(), Im, 0.0001);
     }
 }
