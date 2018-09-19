e4bffdd80f8254219c1151421253337a20722660
Svetlana Reznichenko - 650503 <svetarezrez@gmail.com>
2018-09-19 19:33:00 +0300

    MandelbrotSetTask.java: Equation has been changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c.times(c)).plus(comp).subtraction(comp.times(c)).plus(c);
+            c = c.times(c.times(c).division(comp)).subtraction(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
