dd9ed1eeb3860d1337db7694e95c43cb3328e1e0
Ivan Mazur 650503 <ivan.mazur503@gmail.com>
2018-09-19 21:58:01 +0300

    changed colors

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -310,9 +310,9 @@ class MandelbrotSetTask extends Task<Long> {
         for (int i = 0; i < ANTIALIASING_BASE; i++) {
             for (int j = 0; j < ANTIALIASING_BASE; j++) {
                 Color c = calcPixel(x + step * (i + 0.5) - 0.5, y + step * (j + 0.5) - 0.5);
-                r += c.getRed() / N;
-                g += c.getGreen() / N;
-                b += c.getBlue() / N;
+                r += c.getBlue() / N;
+                g += c.getRed() / N;
+                b += c.getGreen() / N;
             }
         }
         return new Color(clamp(r), clamp(g), clamp(b), 1);
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.LEMONCHIFFON;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.rgb(0, 40, 0),
+                Color.OLIVE,
+                Color.PURPLE,
+                Color.KHAKI,
+                Color.rgb(50, 0, 100),
+                Color.PURPLE,
+                Color.rgb(0, 70, 0)
         };
         
         /**
