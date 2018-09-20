3ce1a6d90cc7a4cc1c212da8b4ae76ae11c2b75e
peekhovsky <peekhovsky@gmail.com>
2018-09-20 11:00:14 +0300

    Change fractal formula

--- src/demo/parallel/Main.java
+++ src/demo/parallel/Main.java
@@ -68,7 +68,6 @@ import javafx.scene.control.ProgressIndicator;
 
 /**
  * UI main class for MandelbrotSet demo.
- * ввмвм
  * <p><i>
  * This source code is provided to illustrate the usage of a given feature
  * or technique and has been deliberately simplified. Additional steps
