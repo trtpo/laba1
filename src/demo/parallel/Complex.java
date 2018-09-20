f90ab1362a11e81777407ec0923c2a25f739d4ca
Andrei Petrov(630501) <43196041+Dogvogol@users.noreply.github.com>
2018-09-20 17:41:57 +0300

    New Fractal change

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,7 +70,16 @@ public class Complex {
         im += b.im;
         return this;
     }
-
+    /**
+     * delete operation.
+     * @param b minmand
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -=b.re;
+        re -=b.im;
+        return this;
+    }
     /**
      * Multiply operation.
      * @param  b multiplier
