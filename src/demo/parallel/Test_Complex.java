86bd3a0e2ca4913c67657102dc5745fbb2a02c77
smotruk <37467892+smotruk@users.noreply.github.com>
2018-09-21 10:24:22 +0300

    add tests

---
+++ src/demo/parallel/Test_Complex.java
@@ -0,0 +1,28 @@
+package demo.parallel;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Test_Complex {
+    Complex a;
+    Complex b;
+    @Before
+    public void Set()
+    {
+        a=new Complex(7,3);
+        b=new Complex(4,-1);
+    }
+    @Test
+    public void Test_minus()
+    {
+        Complex c=a.minus(b);
+        assertEquals(c.getRe(),3.0);
+        assertEquals(c.getIm(),4.0);
+    }
+
+
+}
