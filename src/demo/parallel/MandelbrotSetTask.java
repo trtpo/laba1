07728993bf04ce6f838700871cbf7f5adf31520f
VladislavLiudchyk <los.merengues77@gmail.com>
2018-09-26 00:02:04 +0300

    The color scheme of the image was changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(10, 30, 100),
+            Color.LIGHTGOLDENRODYELLOW,
+            Color.BEIGE,
+            Color.LIGHTGOLDENRODYELLOW,
+            Color.rgb(155, 70, 30),
+            Color.LIGHTGOLDENRODYELLOW,
+            Color.rgb(100, 140, 140)
         };
         
         /**
