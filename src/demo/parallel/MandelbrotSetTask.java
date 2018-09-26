ca90f126a81fa7f4e036bcb4ed721c3d68a8b97a
BogdanVacban <bkurilo363@gmail.com>
2018-09-26 22:08:49 +0300

    New fractal equation.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -277,7 +277,8 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            //c = c.times(c).plus(comp);    // First version
+            c = c.times(c.times(comp).plus(comp));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
