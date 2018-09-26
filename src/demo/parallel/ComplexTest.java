7e2ff3b83b512d8c7079e76217de6afb71db84c0
in-hale <mkrvch.yury@gmail.com>
2018-09-26 08:58:58 +0300

    Improved tests

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -5,26 +5,26 @@ public class ComplexTest {
     private static final double DELTA = 1e-15;
     @Test
     public void testAdd() {
-        Complex a = new Complex(5, 38);
-        Complex b = new Complex(4, 28);
+        Complex a = new Complex(5, 33);
+        Complex b = new Complex(4, 42);
         a.plus(b);
-        assertEquals(a.getReal(),9);
-        assertEquals(a.getImaginary(), 66);
+        assertEquals(a.getReal(),9, "Addition failed");
+        assertEquals(a.getImaginary(), 75, "Addition failed");
     }
     @Test
     public void testMinus() {
-        Complex a = new Complex(6, 8);
-        Complex b = new Complex(10, 4);
+        Complex a = new Complex(6, 22);
+        Complex b = new Complex(11, 4);
         a.substraction(b);
-        assertEquals(a.getReal(), -4);
-        assertEquals(a.getImaginary(), 4);
+        assertEquals(a.getReal(), -5, "Subtraction failed");
+        assertEquals(a.getImaginary(), 18, "Subtraction failed");
     }
     @Test
     public void testTimes() {
-        Complex a = new Complex(6, 8);
+        Complex a = new Complex(8, 8);
         Complex b = new Complex(10, 4);
         a.times(b);
-        assertEquals(a.getReal(), 28);
-        assertEquals(a.getImaginary(), 104);
+        assertEquals(a.getReal(), 48, "Times failed");
+        assertEquals(a.getImaginary(), 112, "Times failed");
     }
 }
