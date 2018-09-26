040e8c2ac77e7a0da726d6de8377a0b5f74c0f8f
Nikita Polgui <nikitapolgui@mail.ru>
2018-09-26 12:37:55 +0300

    The color has been changed from RED to TURQUOISE.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(16, 71, 66),
+            Color.TURQUOISE,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.TURQUOISE,
+            Color.rgb(48, 213, 200),
+            Color.TURQUOISE,
+            Color.rgb(14, 67, 63)
         };
         
         /**
