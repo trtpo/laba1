43c51bfc208515cf20132c7a2d8cd7d7f63f2364
TsyPolina <tsy.polina@gmail.com>
2018-09-27 15:44:42 +0300

    third task

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,19 @@ public class Complex {
         im = imag;
         return this;
     }
+    public Complex minus(Complex b){
+        re -=b.re;
+        im -=b.im;
+        return this;
+    }
+    public Complex division(Complex b){
+        Complex a = this;
+        double real = a.re / b.re - a.im / b.im;
+        double imag = a.re / b.im + a.im / b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
 
     /**
      * Square of Complex object's length, we're using square of length to 
