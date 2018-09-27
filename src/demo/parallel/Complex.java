aa5049decfd1f24ec1a87e5cde1e28cb737cc15a
Sicphy <petrov.kirill.1998@mail.ru>
2018-09-27 16:19:19 +0300

    added some functions for complex numbers and changed fractal's formula

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -72,6 +72,31 @@ public class Complex {
     }
 
     /**
+     * Minus operation.
+     * @param b subtraction
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
+     * Division operation.
+     * @param b divide
+     * @return this Complex object whose value is (this / b)
+     */
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im*b.im);
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
