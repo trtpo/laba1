243120490fbde88c34b42cecce3c98305d660d7b
Vavashka23 <vova.kupreev.23@mail.ru>
2018-09-27 15:58:53 +0300

    Add unit-test file - ComplexTest.java

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,31 @@
+package demo.parallel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComplexTest {
+    private Complex number;
+
+    @Before
+    public void setUp() throws Exception {
+        number = new Complex(2,5);
+    }
+
+    @Test
+    public void minus() throws Exception {
+        number.minus(new Complex(5,6));
+        assertEquals(-3,number.getRe(),0.00001);
+        assertEquals(-1,number.getIm(),0.00001);
+
+    }
+
+    @Test
+    public void times() throws Exception {
+        number.times(new Complex(5,6));
+        assertEquals(-20,number.getRe(),0.00001);
+        assertEquals(37,number.getIm(),0.00001);
+    }
+
+}
