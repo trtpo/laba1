407e7a37b0c365d2238b4201b697c2e74f4a35e1
ZhekaKZ <ZhekasKZ>
2018-09-28 15:58:09 +0300

    Изменить уравнение и добавил функцию минус

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,7 +70,11 @@ public class Complex {
         im += b.im;
         return this;
     }
-
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
     /**
      * Multiply operation.
      * @param  b multiplier
