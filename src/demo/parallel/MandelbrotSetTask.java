54a180d0043b0423e34eae168129463f070bfd5c
Ilya Belevich <ilya.belevich@mail.ru>
2018-09-30 17:01:49 +0300

    Changing background color

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(80, 40, 80),
+            Color.FORESTGREEN,
+            Color.PURPLE,
+            Color.YELLOW,
+            Color.rgb(50, 80, 90),
+            Color.YELLOW,
+            Color.rgb(20, 80, 40)
         };
         
         /**
