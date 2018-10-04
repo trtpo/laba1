078948878c5fc41c414b3514f396fb01c66f15b1
Кирилл <kiril.novikov.2222@mail.ru>
2018-10-04 23:37:33 +0300

    update fractal

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,32 @@ public class Complex {
     }
 
     /**
+     * Sub operation
+     * @param b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex minus(Complex b)
+    {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+    /**
+     *
+     * @param b dividend
+     * @return  this Complex object whose value is this /b
+     */
+    public Complex delete(Complex b)
+    {
+        Complex a = this;
+        double real=(a.re*b.re+a.im*b.im)/(Math.pow(b.re,2)+Math.pow(b.im,2));
+        double imag= (a.im*b.re-a.re*b.im)/(Math.pow(b.re,2)+Math.pow(b.im,2));
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
