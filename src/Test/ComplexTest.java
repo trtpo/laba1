5f45380fc0050030e702124fe2ce19ac5fed3887
MikhailPopovWNN <m.popovwnn@gmail.com>
2018-10-09 22:41:25 +0300

    Testclass is fixed.

--- src/Test/ComplexTest.java
+++ src/Test/ComplexTest.java
@@ -8,45 +8,35 @@ public class ComplexTest {
 
     @org.junit.Test
     public void minus() throws Exception {
-        double reA = 5.5;
-        double imA = 6.7;
-        double reB = 1.3;
-        double imB = 2.2;
-        Complex a = new Complex(reA,imA);
-        Complex b = new Complex(reB,imB);
-        Complex res = new Complex(reA - reB, imA - imB);
-        assertTrue("Test minus() is failed",res.equalsTo(a.minus(b)));
+        Complex a = new Complex(2, 5);
+        Complex c = new Complex(6, 9);
+        assertEquals( "Test minus() is failed", a.minus(new Complex(1, 3)), new Complex(1,2));
+        assertEquals( "Test minus() is failed", c.minus(new Complex(3, 3)), new Complex(3,6));
     }
 
     @org.junit.Test
     public void sin() throws Exception {
-        double reA = 5.5;
-        double imA = 6.7;
-        Complex a = new Complex(reA,imA);
-        Complex res = new Complex(Math.sin(reA) * Math.cosh(imA), Math.cos(reA) * Math.sinh(imA));
-        assertTrue("Test sin() is failed", res.equalsTo(a.sin()));
+        Complex a = new Complex(11, 0);
+        Complex b = new Complex(1, 0);
+        assertEquals("Test sin() is failed", a.sin(), new Complex(-0.9999902065507035,0));
+        assertEquals("Test sin() is failed", b.sin(), new Complex(0.8414709848078965,0));
     }
 
     @org.junit.Test
     public void cos() throws Exception {
-        double reA = 5.5;
-        double imA = 6.7;
-        Complex a = new Complex(reA,imA);
-        Complex res = new Complex(Math.cos(reA) * Math.cosh(imA), -Math.sin(reA) * Math.sinh(imA));
-        assertTrue("Test cos() is failed", res.equalsTo(a.cos()));
+        Complex a = new Complex(2, 1);
+        Complex b = new Complex(3,3);
+        assertEquals("Test cos() is failed", a.cos(), new Complex(-0.6421481247155201,-1.0686074213827783));
+        assertEquals("Test cos() is failed", b.cos(), new Complex(-9.966909834129453,-1.413722590498827));
     }
 
-
     @org.junit.Test
-    public void div() throws Exception {
-        double reA = 5.5;
-        double imA = 6.7;
-        double reB = 1.3;
-        double imB = 2.2;
-        double d = reA * reB + imA * imB;
-        Complex a = new Complex(reA,imA);
-        Complex b = new Complex(reB,imB);
-        Complex res = new Complex((reA * reB + imA * imB)/d, (reB * imA - reA * imB)/d);
-        assertTrue("Test div() is failed", res.equalsTo(a.div(b)));
+    public void divide() throws Exception {
+        Complex a = new Complex(3,4);
+        Complex b = new Complex(2,1);
+        Complex c = new Complex(6,2);
+        Complex d = new Complex(4,2);
+        assertEquals( "Test divide() is failed", a.divide(b), new Complex(2,1));
+        assertEquals( "Test divide() is failed", c.divide(d), new Complex(1.4,-0.2));
     }
 }
