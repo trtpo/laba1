b3a7894628ed31431c39511482a453fbdc581733
Alexander Zuzkin <alexander.zuz650501@gmail.com>
2018-10-25 15:31:20 +0300

    background gamma changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.GOLD;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.WHITE,
+                Color.rgb(23,250,32),
+                Color.WHITE,
+                Color.RED,
+                Color.rgb(27, 183, 163),
+                Color.BLUE,
+                Color.rgb(196, 112, 177)
         };
         
         /**
