0ac09d0a2673c2f6b174b4b35103e675fbbd7e15
AliakseiPischyk <aliaksei.pischyk@gmail.com>
2019-09-05 13:02:05 +0300

    Changed color spectrum.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(40, 100, 100),
+            Color.BLUE,
             Color.WHITE,
+            Color.BLACK,
+            Color.rgb(100, 100, 100),
             Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(50, 200, 100)
         };
         
         /**
