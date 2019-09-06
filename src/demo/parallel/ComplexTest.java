e007e8fd43e42a9b6a84550ebff049003ff87b2a
AliakseiPischyk <aliaksei.pischyk@gmail.com>
2019-09-06 13:26:40 +0300

    Removed floating point values comparator. Found a junit method that compares floating point values with given accuracy.

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -9,7 +9,7 @@ public class ComplexTest {
     static private final double epsilon = 0.01;
 
     @org.junit.Before
-    public void setUp() throws Exception {
+    public void setUp() {
         a = new Complex(1,1);
         b = new Complex(2,2);
     }
@@ -17,42 +17,42 @@ public class ComplexTest {
     @org.junit.Test
     public void modATest() {
         double modA = a.mod();
-        assertTrue(FloatingComparator.checkIfEquals(1.4142136,modA,epsilon));
+        assertEquals(1.4142136,modA,epsilon);
     }
 
     @org.junit.Test
     public void conjugateATest() {
         Complex conjugateAResult = a.conjugate();
-        assertTrue(FloatingComparator.checkIfEquals(conjugateAResult.getIm(),-1.0,epsilon));
-        assertTrue(FloatingComparator.checkIfEquals(conjugateAResult.getRe(),1.0,epsilon));
+        assertEquals(conjugateAResult.getIm(),-1.0,epsilon);
+        assertEquals(conjugateAResult.getRe(),1.0,epsilon);
     }
 
     @org.junit.Test
     public void divideAByB() {
         Complex resultADividedByB = Complex.divide(a,b);
-        assertTrue(FloatingComparator.checkIfEquals(resultADividedByB.getIm(),0.0,epsilon));
-        assertTrue(FloatingComparator.checkIfEquals(resultADividedByB.getRe(),0.5,epsilon));
+        assertEquals(resultADividedByB.getIm(),0.0,epsilon);
+        assertEquals(resultADividedByB.getRe(),0.5,epsilon);
     }
 
     @org.junit.Test
     public void sinOfA() {
         Complex sinA = Complex.sin(a);
-        assertTrue(FloatingComparator.checkIfEquals(sinA.getIm(),0.63496391,epsilon));
-        assertTrue(FloatingComparator.checkIfEquals(sinA.getRe(),1.29845758,epsilon));
+        assertEquals(sinA.getIm(),0.63496391,epsilon);
+        assertEquals(sinA.getRe(),1.29845758,epsilon);
 
     }
 
     @org.junit.Test
     public void cosOfA() {
         Complex cosA = Complex.cos(a);
-        assertTrue(FloatingComparator.checkIfEquals(cosA.getIm(),-0.98889771,epsilon));
-        assertTrue(FloatingComparator.checkIfEquals(cosA.getRe(),0.83373003,epsilon));
+        assertEquals(cosA.getIm(),-0.98889771,epsilon);
+        assertEquals(cosA.getRe(),0.83373003,epsilon);
     }
 
     @org.junit.Test
     public void tanOfA() {
         Complex tanA = Complex.tan(a);
-        assertTrue(FloatingComparator.checkIfEquals(tanA.getIm(),1.08392333,epsilon));
-        assertTrue(FloatingComparator.checkIfEquals(tanA.getRe(),0.27175259,epsilon));
+        assertEquals(tanA.getIm(),1.08392333,epsilon);
+        assertEquals(tanA.getRe(),0.27175259,epsilon);
     }
 }
