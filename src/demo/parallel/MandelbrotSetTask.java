4541b07e5649beff1849d09193b4a9fa5ee45f49
s1ovak <slava_muhin@tut.by>
2019-09-12 13:16:29 +0300

    New operations for complex number & new fractal equation

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).times(c).times(c).plus(comp).plus(c);
+            c = c.times(c).times(c).times(c).divide(c.sin().plus(comp.cos())).plus(comp).plus(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
