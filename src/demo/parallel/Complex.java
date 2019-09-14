7464f98416952a912851cbacbc36b0cc910cfd58
Vlad Rublevskiy <vladislav.rublevskiy@gmail.com>
2019-09-14 17:44:27 +0300

    Add new operations for a complex number

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,23 @@ public class Complex {
     }
 
     /**
+     * Get parts of number in algebraic form
+     * @return real part of Complex object
+     */
+    public double getRe(){
+        return re;
+    }
+
+    /**
+     * Get parts of number in algebraic form
+     * @return imaginary part of Complex object
+     */
+    public double getIm(){
+        return im;
+    }
+
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
