26a626fecbac6d16cfa85784f0349f16268941b5
ceyz <ceyz@list.ru>
2019-09-15 22:49:57 +0300

    Second commit!Changed fractal formula, added division method for Complex class.

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,26 @@ public class Complex {
         return this;
     }
 
+    public Complex divide(Complex b) {
+        if(b.re==0 && b.im==0)
+        {
+            throw new ArithmeticException("Error! Delenie na 0");
+        }
+        if((b.re*b.re+b.im*b.im)!=0)
+        {
+            Complex a=this;
+            double real=(a.re*b.re+a.im*b.im)/(b.re*b.re+b.im*b.im);
+            double imag=(b.re*a.im-a.re*b.im)/(b.re*b.re+b.im*b.im);
+            a.re=real;
+            a.im=imag;
+        }
+        else
+        {
+            throw new ArithmeticException("Error! Delenie na 0");
+        }
+        return this;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
