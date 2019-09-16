120d34d1b5bac89118a2d20d1d691c3004f8e9be
AlPsvch <pasevich.aleksej@gmail.com>
2019-09-16 21:38:58 +0300

    Fractal equation changed with new operations

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -277,7 +277,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).divide(c.cos().minus(c.sin().times(comp))).times(c).minus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
