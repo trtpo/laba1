85ba8273aecb27052dd7150bbef74a5ca19ec6cd
Dima Alhovik <halapsikon@mail.ru>
2019-09-17 01:04:37 +0300

    functionality expanded

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,21 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b){
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public Complex div(Complex b){
+        Complex a=this;
+        double real = (a.re*b.re+a.im*b.im)/(b.re*b.re+b.im*b.im);
+        double imag = (a.im*b.re-a.re*b.im)/(b.re*b.re+b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
