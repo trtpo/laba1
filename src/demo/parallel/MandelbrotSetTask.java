da3962e23d49230ea71fcf529bb5f8593e719cca
IlyaKolbasov <51168504+IlyaKolbasov@users.noreply.github.com>
2019-09-16 19:24:22 +0300

    Цветовая гамма изображения изменена!

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -352,11 +352,11 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
+            Color.GREEN,
+            Color.BLUE,
             Color.RED,
             Color.rgb(100, 0, 0),
-            Color.RED,
+            Color.BLACK,
             Color.rgb(50, 0, 0)
         };
         
