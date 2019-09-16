928d7df0bfca3e87d2b1dd2ea32c7971fce903f0
Makar <mr.makar12@gmail.com>
2019-09-16 20:27:35 +0300

    commit_3_modified fractal equation with new formulas (my excuses for loosing one point)

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -45,15 +45,14 @@ package demo.parallel;
  * @author Alexander Kouznetsov, Tristan Yan
  */
 public class Complex {
-    
-    private double re;   // the real part
-    private double im;   // the imaginary part
+    private final double re;   // the real part
+    private final double im;   // the imaginary part
 
-    /** 
-     * create a new object with the given real and imaginary parts
-     * 
-     * @param real a complex number real part
-     * @param imag a complex number imaginary part 
+    /**
+     * Initializes a complex number from the specified real and imaginary parts.
+     *
+     * @param real the real part
+     * @param imag the imaginary part
      */
     public Complex(double real, double imag) {
         re = real;
@@ -61,35 +60,185 @@ public class Complex {
     }
 
     /**
-     * Add operation.
-     * @param b summand
-     * @return this Complex object whose value is (this + b)
+     * Returns a string representation of this complex number.
+     *
+     * @return a string representation of this complex number,
+     *         of the form 34 - 56i.
      */
-    public Complex plus(Complex b) {
-        re += b.re;
-        im += b.im;
-        return this;
+    public String toString() {
+        if (im == 0) return re + "";
+        if (re == 0) return im + "i";
+        if (im <  0) return re + " - " + (-im) + "i";
+        return re + " + " + im + "i";
     }
 
     /**
-     * Multiply operation.
-     * @param  b multiplier
-     * @return this Complex object whose value is this * b
+     * Returns the absolute value of this complex number.
+     * This quantity is also known as the <em>modulus</em> or <em>magnitude</em>.
+     *
+     * @return the absolute value of this complex number
      */
-    public Complex times(Complex b) {
-        Complex a = this;
-        double real = a.re * b.re - a.im * b.im;
-        double imag = a.re * b.im + a.im * b.re;
-        re = real;
-        im = imag;
-        return this;
+    public double abs() {
+        return Math.hypot(re, im);
     }
 
     /**
-     * Square of Complex object's length, we're using square of length to 
-     * eliminate the computation of square root
-     * @return square of length
-    */
+     * Returns the phase of this complex number.
+     * This quantity is also known as the <em>angle</em> or <em>argument</em>.
+     *
+     * @return the phase of this complex number, a real number between -pi and pi
+     */
+    public double phase() {
+        return Math.atan2(im, re);
+    }
+
+    /**
+     * Returns the sum of this complex number and the specified complex number.
+     *
+     * @param  that the other complex number
+     * @return the complex number whose value is {@code (this + that)}
+     */
+    public Complex plus(Complex that) {
+        double real = this.re + that.re;
+        double imag = this.im + that.im;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Returns the result of subtracting the specified complex number from
+     * this complex number.
+     *
+     * @param  that the other complex number
+     * @return the complex number whose value is {@code (this - that)}
+     */
+    public Complex minus(Complex that) {
+        double real = this.re - that.re;
+        double imag = this.im - that.im;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Returns the product of this complex number and the specified complex number.
+     *
+     * @param  that the other complex number
+     * @return the complex number whose value is {@code (this * that)}
+     */
+    public Complex times(Complex that) {
+        double real = this.re * that.re - this.im * that.im;
+        double imag = this.re * that.im + this.im * that.re;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Returns the product of this complex number and the specified scalar.
+     *
+     * @param  alpha the scalar
+     * @return the complex number whose value is {@code (alpha * this)}
+     */
+    public Complex scale(double alpha) {
+        return new Complex(alpha * re, alpha * im);
+    }
+
+    /**
+     * Returns the product of this complex number and the specified scalar.
+     *
+     * @param  alpha the scalar
+     * @return the complex number whose value is {@code (alpha * this)}
+     * @deprecated Replaced by {@link #scale(double)}.
+     */
+    @Deprecated
+    public Complex times(double alpha) {
+        return new Complex(alpha * re, alpha * im);
+    }
+
+    /**
+     * Returns the complex conjugate of this complex number.
+     *
+     * @return the complex conjugate of this complex number
+     */
+    public Complex conjugate() {
+        return new Complex(re, -im);
+    }
+
+    /**
+     * Returns the reciprocal of this complex number.
+     *
+     * @return the complex number whose value is {@code (1 / this)}
+     */
+    public Complex reciprocal() {
+        double scale = re*re + im*im;
+        return new Complex(re / scale, -im / scale);
+    }
+
+    /**
+     * Returns the real part of this complex number.
+     *
+     * @return the real part of this complex number
+     */
+    public double re() {
+        return re;
+    }
+
+    /**
+     * Returns the imaginary part of this complex number.
+     *
+     * @return the imaginary part of this complex number
+     */
+    public double im() {
+        return im;
+    }
+
+    /**
+     * Returns the result of dividing the specified complex number into
+     * this complex number.
+     *
+     * @param  that the other complex number
+     * @return the complex number whose value is {@code (this / that)}
+     */
+    public Complex divides(Complex that) {
+        return this.times(that.reciprocal());
+    }
+
+    /**
+     * Returns the complex exponential of this complex number.
+     *
+     * @return the complex exponential of this complex number
+     */
+    public Complex exp() {
+        return new Complex(Math.exp(re) * Math.cos(im), Math.exp(re) * Math.sin(im));
+    }
+
+    /**
+     * Returns the complex sine of this complex number.
+     *
+     * @return the complex sine of this complex number
+     */
+    public Complex sin() {
+        return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
+    }
+
+    /**
+     * Returns the complex cosine of this complex number.
+     *
+     * @return the complex cosine of this complex number
+     */
+    public Complex cos() {
+        return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
+    }
+
+    /**
+     * Returns the complex tangent of this complex number.
+     *
+     * @return the complex tangent of this complex number
+     */
+    public Complex tan() {
+        return sin().divides(cos());
+    }
+
+
+
+
+
     public double lengthSQ() {
         return re * re + im * im;
     }
