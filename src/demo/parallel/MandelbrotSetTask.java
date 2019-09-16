7491cddbfb6ad01fd8483843b568aace237df720
Aleksey Pugach <pugach.aleksey@mail.ru>
2019-09-16 18:31:47 +0300

    Update MandelbrotSetTask.java

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -40,7 +40,7 @@ import javafx.scene.paint.Color;
 
 /**
  * Task to render Mandelbrot set using given parameters. See {@link 
- * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
+ * #MandelbrotRendererTask(boolean, PixelWriter, int, int,
  * double, double, double, double, double, double, double, double, boolean) 
  * constructor} for parameters list. The task returns time in milliseconds as 
  * its calculated value.
@@ -218,7 +218,7 @@ class MandelbrotSetTask extends Task<Long> {
             // Prepares an image 
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
-                    pixelWriter.setColor(x, y, Color.TRANSPARENT);
+                    pixelWriter.setColor(x, y, Color.YELLOW);
                 }
             }
         }
@@ -283,7 +283,7 @@ class MandelbrotSetTask extends Task<Long> {
 
     /**
      * Calculates a color of a given pixel on the image using 
-     * {@link #calc(demo.parallel.Complex) } method.
+     * {@link #calc(Complex) } method.
      * @param x x coordinate of the pixel in the image
      * @param y y coordinate of the pixel in the image
      * @return calculated color of the pixel
@@ -330,7 +330,7 @@ class MandelbrotSetTask extends Task<Long> {
     /**
      * Returns a color for a given iteration count.
      * @param count number of iterations return by 
-     * {@link #calc(demo.parallel.Complex)} method
+     * {@link #calc(Complex)} method
      * @return color from pre-calculated table
      */
     private Color getColor(int count) {
@@ -352,11 +352,11 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.BLUE,
             Color.WHITE,
-            Color.RED,
+            Color.BLUE,
             Color.rgb(100, 0, 0),
-            Color.RED,
+            Color.BLUE,
             Color.rgb(50, 0, 0)
         };
         
