8eaa3aeeaf85d4ee253e7e2b6d89d5b882523f0e
Igor Tarasyuk <49386457+eBlow@users.noreply.github.com>
2019-09-16 18:52:35 +0300

    Equation edited

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -32,6 +32,7 @@
 
 package demo.parallel;
 
+
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 import javafx.concurrent.Task;
@@ -276,7 +277,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).plus(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
