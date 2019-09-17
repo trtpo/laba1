49050e58ea3b40ca87c2476c85e1672ac5eb9afc
artimendor <artimendor@yandex.ru>
2019-09-17 13:56:18 +0300

    added operations:  * minus (a - b)  * division (a / b)
    formula changed:
    
    Z^3 + Z^2 + c  to  Z^3 + Z^2 + c + (1 - c)/Z

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,34 @@ public class Complex {
     }
 
     /**
+     * minus operation.
+     * @param b operand to minus
+     * @return this Complex object whose value is this - b
+     */
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re - b.re;
+        double imag = a.im - b.im;
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
+     * division operation
+     * @param b divider
+     * @return this complex objecy whose value is divided by b
+     */
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double imag = (a.re*b.re + a.im*b.im) / (b.re*b.re + b.im*b.im);
+        double real = (b.re*a.im - a.re*b.im) / (b.re*b.re + b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,8 +275,8 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-//            c = c.times(c).plus(comp);
-            c = c.times(c).times(c).plus(c.times(c)).plus(comp);
+            c = c.times(c).times(c).plus(c.times(c)).plus(comp)
+                    .plus((new Complex(1,0)).minus(comp).divide(c));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
