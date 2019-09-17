791a0b27fe688c9d573c44fb62ac2a58b8e2c45f
Alexey Danilchyck <danilchyck@gmail.com>
2019-09-18 01:03:51 +0300

    Division and subtraction complex number operations added and implemented

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,31 @@ public class Complex {
     }
 
     /**
+     * Divide operation.
+     * @param b divisor
+     * @return this Complex object whose value is this / b
+     */
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re*b.re + a.im*b.im) / (b.re*b.re + b.im*b.im);
+        double imag = (a.im*b.re - a.re*b.im) / (b.re*b.re + b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
+     * Subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
