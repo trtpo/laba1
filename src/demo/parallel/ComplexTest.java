2264631c97b4d1b043d24b46e3bb5f4f1051bb76
akhramowicz <irenakalin2000@gmail.com>
2019-09-19 01:09:15 +0300

    tests

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,30 @@
+package demo.parallel;
+
+import org.junit.Assert;
+
+
+public class ComplexTest extends Assert {
+
+    @org.junit.Test
+    public void firstSubtractionTest() {
+        Complex comp = new Complex(5, 7);
+        assertTrue(new Complex(2, 3).equal(comp.sub(new Complex(3, 4))));
+    }
+
+    @org.junit.Test
+    public void secondSubtractionTest() {
+        Complex comp = new Complex(4, 7);
+        assertTrue(new Complex(1, -1).equal(comp.sub(new Complex(3, 8))));
+    }
+    @org.junit.Test
+    public void firstDivisionTest(){
+        Complex comp = new Complex(5, 7);
+        assertTrue(new Complex(2.2, 1.6).equal(comp.div(new Complex(3, 1))));
+    }
+    @org.junit.Test
+    public void secondDivisionTest(){
+        Complex comp = new Complex(5, -7);
+        assertTrue(new Complex((0.8), (-2.6)).equal(comp.div(new Complex(3, 1))));
+    }
+
+}
