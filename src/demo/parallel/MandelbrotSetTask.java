a8d9d511ef1223c37f07c87aa8f6f01885650305
Asus <gricuk112@gmail.com>
2019-09-19 14:50:26 +0300

    color

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.CHOCOLATE;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
+            Color.rgb(63, 215, 24),
             Color.RED,
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
+            Color.rgb(0, 150, 150),
             Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(160, 200, 0)
         };
         
         /**
