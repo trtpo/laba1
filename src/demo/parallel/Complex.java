b2c2aafc4fa01f4bb97803a3e983db0631b6af7a
Dzmitry Kuhach 750503 <dkaezi@gmail.com>
2019-09-21 19:13:39 +0300

    Added new operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -42,18 +42,19 @@ package demo.parallel;
  * required for a production-quality application, such as security checks,
  * input validation and proper error handling, might not be present in
  * this sample code.</i>
+ *
  * @author Alexander Kouznetsov, Tristan Yan
  */
 public class Complex {
-    
+
     private double re;   // the real part
     private double im;   // the imaginary part
 
-    /** 
+    /**
      * create a new object with the given real and imaginary parts
-     * 
+     *
      * @param real a complex number real part
-     * @param imag a complex number imaginary part 
+     * @param imag a complex number imaginary part
      */
     public Complex(double real, double imag) {
         re = real;
@@ -62,6 +63,7 @@ public class Complex {
 
     /**
      * Add operation.
+     *
      * @param b summand
      * @return this Complex object whose value is (this + b)
      */
@@ -71,9 +73,26 @@ public class Complex {
         return this;
     }
 
+    public Complex divide(Complex b) {
+        if (b.re == 0 && b.im == 0) {
+            throw new ArithmeticException("Error! You can't divide by zero");
+        }
+        if ((b.re * b.re + b.im * b.im) != 0) {
+            Complex a = this;
+            double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im);
+            double imag = (b.re * a.im - a.re * b.im) / (b.re * b.re + b.im * b.im);
+            a.re = real;
+            a.im = imag;
+        } else {
+            throw new ArithmeticException("Error! You can't divide by zero");
+        }
+        return this;
+    }
+
     /**
      * Multiply operation.
-     * @param  b multiplier
+     *
+     * @param b multiplier
      * @return this Complex object whose value is this * b
      */
     public Complex times(Complex b) {
@@ -86,10 +105,11 @@ public class Complex {
     }
 
     /**
-     * Square of Complex object's length, we're using square of length to 
+     * Square of Complex object's length, we're using square of length to
      * eliminate the computation of square root
+     *
      * @return square of length
-    */
+     */
     public double lengthSQ() {
         return re * re + im * im;
     }
