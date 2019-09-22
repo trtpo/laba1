7375690479ea6e36d5bed87a34aab937b97f4f78
ptaxom <ptaxom@gmail.com>
2019-09-22 21:32:49 +0300

    Changed color gamma.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.GREEN,
+                Color.GREENYELLOW,
+                Color.YELLOW,
+                Color.rgb(170, 200,  255),
+                Color.VIOLET,
+                Color.BLUEVIOLET,
+                Color.BLUE
         };
         
         /**
