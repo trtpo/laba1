5373b9ef098a9a18bad228934d269d0f20ac9bdc
Naumenko Andrew <naymenko100@gmail.com>
2019-09-22 09:37:14 +0300

    new colors

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.THISTLE;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(29, 16, 24),
+            Color.VIOLET,
+            Color.WHEAT,
+            Color.AQUA,
+            Color.rgb(110, 107, 130),
+            Color.TAN,
+            Color.rgb(189, 187, 199)
         };
         
         /**
