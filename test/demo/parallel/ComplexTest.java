e6f87f573622f2ffe271fe857e4448000b1ac242
MurphyDM <diana.shelotkach@mail.ru>
2019-09-23 15:23:34 +0300

    add add divide and minus tests

--- test/demo/parallel/ComplexTest.java
+++ test/demo/parallel/ComplexTest.java
@@ -91,4 +91,73 @@ public class ComplexTest {
         );
     }
 
+    @Test
+    public void divideTest() {
+
+        Complex a = new Complex(12, -3);
+        Complex b = new Complex(3, 3);
+
+        a.divide(b);
+
+        collector.checkThat(
+                "Division operation test. Wrong real part result.",
+                a.getRe() != 1.5 && a.getIm() == -2.5,
+                equalTo(false)
+        );
+        collector.checkThat(
+                "Division operation test. Wrong imaginary part result.",
+                a.getRe() == 1.5 && a.getIm() != -2.5,
+                equalTo(false)
+        );
+        collector.checkThat(
+                "Division operation test. Wrong real and imaginary part result.",
+                a.getRe() != 1.5 && a.getIm() != -2.5,
+                equalTo(false)
+        );
+    }
+
+    @Test
+    public void minusTestPositive() {
+
+        Complex a = new Complex(12, 9);
+        Complex subtrahend = new Complex(12, -7);
+
+        a.minus(subtrahend);
+
+        collector.checkThat(
+                "Subtraction operation test. Wrong result.",
+                a.equals(new Complex(0, 16)),
+                equalTo(true)
+        );
+    }
+
+    @Test
+    public void minusTestNegative() {
+
+        Complex b = new Complex(-18, -1);
+        Complex subtrahend = new Complex(12, -7);
+
+        b.minus(subtrahend);
+
+        collector.checkThat(
+                "Subtraction operation test. Wrong result.",
+                b.equals(new Complex(-30, 6)),
+                equalTo(true)
+        );
+    }
+
+    @Test
+    public void minusTestZero() {
+
+        Complex c = new Complex(12, -3);
+        Complex subtrahend = new Complex(12, -3);
+
+        c.minus(subtrahend);
+
+        collector.checkThat(
+                "Subtraction operation test. Wrong result.",
+                c.equals(new Complex(0, 0)),
+                equalTo(true)
+        );
+    }
 }
