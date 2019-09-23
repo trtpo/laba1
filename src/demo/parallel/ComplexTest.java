b192f1914de53b67128ac16aa38fe2998be9ead9
KevinPozitive <marshal.vlad.99@gmail.com>
2019-09-24 00:39:59 +0300

    CreateDivTest

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,19 @@
+package demo.parallel;
+
+import static org.junit.Assert.*;
+
+public class ComplexTest {
+
+    @org.junit.Test
+    public void ComplexNoDivZero() {
+        Complex testNum1 = new Complex(0, 0);
+        double divRe=testNum1.getReal();
+        double divImg=testNum1.getImg();
+
+        if (divRe==0 || divImg==0)
+        {
+            System.out.println("Cannot be divided by zero!");
+        }
+        else {System.out.println("so good");}
+    }
+}
