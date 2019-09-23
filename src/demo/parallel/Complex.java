6b6e3ff122bd2442abdc1ae1984900205280c103
Galyuk Gleb, 750501 <galukgleb@gmail.com>
2019-09-24 01:04:17 +0300

    new operations are added and applied

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -72,14 +72,45 @@ public class Complex {
     }
 
     /**
+     * Subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Multiply operation.
      * @param  b multiplier
-     * @return this Complex object whose value is this * b
+     * @return this Complex object whose value is (this * b)
      */
     public Complex times(Complex b) {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
         double imag = a.re * b.im + a.im * b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
+     * Divide operation.
+     * @param b divider
+     * @return this Complex object whose value is (this / b)
+     */
+    public Complex divide(Complex b) {
+        if (b.lengthSQ() == 0) {
+            return this;
+        }
+
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) /
+                (Math.pow(b.re, 2) + Math.pow(b.im, 2));
+        double imag = (a.im * b.re - a.re * b.im) /
+                (Math.pow(b.re, 2) + Math.pow(b.im, 2));
         re = real;
         im = imag;
         return this;
