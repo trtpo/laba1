757c2cdd651832a64267fe51ab474176c4a62f7e
Speedy3004 <redddd15@gmail.com>
2019-09-23 22:16:51 +0300

    add minus operation and change the fractal equation by minus operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,17 @@ public class Complex {
     }
 
     /**
+     * Minus operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
