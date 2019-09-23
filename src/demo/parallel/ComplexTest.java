87779d0d8715efd955f62af22227e15ae708b5f5
Speedy3004 <redddd15@gmail.com>
2019-09-24 00:16:25 +0300

    fix ComplexTest

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class ComplexTest {
 
     @Test
-    public void ComplexMinusEqualsZeroTest() throws Exception{
+    public void ComplexMinusEqualsZeroTest() {
         Complex firstVariable = new Complex(3.8, 7.2);
         Complex secondVariable = new Complex( 3.8, 7.2);
         Complex actualResult = firstVariable.minus(secondVariable);
 
-        Complex expectedResult = new Complex(0.0,0.0);
+        Complex expectedResult = new Complex(0.0, 0.0);
 
         double delta = 0.001;
 
@@ -21,12 +21,12 @@ public class ComplexTest {
     }
 
     @Test
-    public void ComplexMinusPositiveReImTest() throws Exception{
+    public void ComplexMinusPositiveReImTest() {
         Complex firstVariable = new Complex(4.976, 9.937);
         Complex secondVariable = new Complex( 6.119, -11.248);
         Complex actualResult = firstVariable.minus(secondVariable);
 
-        Complex expectedResult = new Complex(-1.143,21.185);
+        Complex expectedResult = new Complex(-1.143, 21.185);
 
         double delta = 0.001;
 
@@ -35,12 +35,12 @@ public class ComplexTest {
     }
 
     @Test
-    public void ComplexMinusNegativeReImTest() throws Exception{
+    public void ComplexMinusNegativeReImTest() {
         Complex firstVariable = new Complex(-17.483, -34.821);
         Complex secondVariable = new Complex( -19.849, 19.894);
         Complex actualResult = firstVariable.minus(secondVariable);
 
-        Complex expectedResult = new Complex(2.366,-54.715);
+        Complex expectedResult = new Complex(2.366, -54.715);
 
         double delta = 0.001;
 
@@ -49,12 +49,12 @@ public class ComplexTest {
     }
 
     @Test
-    public void ComplexMinusPositiveNegativeReImTest() throws Exception{
+    public void ComplexMinusPositiveNegativeReImTest() {
         Complex firstVariable = new Complex(22.792, -27.484);
         Complex secondVariable = new Complex( -14.914, -22.373);
         Complex actualResult = firstVariable.minus(secondVariable);
 
-        Complex expectedResult = new Complex(37.706,-5.111);
+        Complex expectedResult = new Complex(37.706, -5.111);
 
         double delta = 0.001;
 
@@ -63,12 +63,12 @@ public class ComplexTest {
     }
 
     @Test
-    public void ComplexMinusNegativePositiveReImTest() throws Exception{
+    public void ComplexMinusNegativePositiveReImTest() {
         Complex firstVariable = new Complex(-39.293, 42.247);
         Complex secondVariable = new Complex( 59.305, 25.962);
         Complex actualResult = firstVariable.minus(secondVariable);
 
-        Complex expectedResult = new Complex(-98.598,16.285);
+        Complex expectedResult = new Complex(-98.598, 16.285);
 
         double delta = 0.001;
 
