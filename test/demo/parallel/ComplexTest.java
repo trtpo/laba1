151e148ebc40fed4affa72beb9ea59dbdac0eab8
MurphyDM <diana.shelotkach@mail.ru>
2019-09-23 15:19:50 +0300

    add real part tests

---
+++ test/demo/parallel/ComplexTest.java
@@ -0,0 +1,58 @@
+package demo.parallel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class ComplexTest {
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Before
+    public void setup() {}
+
+    @After
+    public void destroy() {}
+
+    @Test
+    public void getReTestZero() {
+
+        Complex a = new Complex(0, 0);
+
+        collector.checkThat(
+                "Getting real part operation test. Wrong result.",
+                a.getRe(),
+                equalTo(0.0)
+        );
+    }
+
+    @Test
+    public void getReTestPositive() {
+
+        Complex b = new Complex(10,0);
+
+        collector.checkThat(
+                "Getting real part operation test. Wrong result.",
+                b.getRe(),
+                equalTo(10.0)
+        );
+    }
+
+    @Test
+    public void getReTestNegative() {
+
+        Complex c = new Complex(-10, 0);
+
+        collector.checkThat(
+                "Getting real part operation test. Wrong result.",
+                c.getRe(),
+                equalTo(-10.0)
+        );
+    }
+
+}
