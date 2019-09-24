c129bb072409276a4191a7f68e34b18a8d8e0779
danila16030 <danila.chechitov1603@gmail.com>
2019-09-24 12:28:38 +0300

    Added two getters

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -112,4 +112,12 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+
+    public double getIm() {
+        return im;
+    }
+
+    public double getRe() {
+        return re;
+    }
 }
