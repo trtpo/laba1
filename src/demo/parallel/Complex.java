4e67df4b9ff89b6bd25155786ee49925a24d441b
Speedy3004 <redddd15@gmail.com>
2019-09-24 14:11:32 +0300

    add division operation and change factorial equation by division operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -88,6 +88,17 @@ public class Complex {
     }
 
     /**
+     * Minus operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
@@ -102,13 +113,16 @@ public class Complex {
     }
 
     /**
-     * Minus operation.
-     * @param b subtrahend
-     * @return this Complex object whose value is this - b
+     * Divide operation.
+     * @param b divider
+     * @return this Complex object whose value is (this / b)
      */
-    public Complex minus(Complex b) {
-        re -= b.re;
-        im -= b.im;
+    public Complex division(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re*b.re+b.im*b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(b.re*b.re+b.im*b.im);
+        re = real;
+        im = imag;
         return this;
     }
 
--- tests/complex/test/ComplexTest.java
+++ tests/complex/test/ComplexTest.java
@@ -1,5 +1,6 @@
-package demo.parallel;
+package complex.test;
 
+import demo.parallel.Complex;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,70 +8,75 @@ import static org.junit.Assert.assertEquals;
 public class ComplexTest {
 
     @Test
-    public void ComplexMinusEqualsZeroTest() {
-        Complex firstVariable = new Complex(3.8, 7.2);
-        Complex secondVariable = new Complex( 3.8, 7.2);
+    public void complexMinusEqualsZeroTest() {
+        Complex firstVariable = new Complex(3.886, 7.271);
+        Complex secondVariable = new Complex(3.886, 7.271);
+
         Complex actualResult = firstVariable.minus(secondVariable);
 
         Complex expectedResult = new Complex(0.0, 0.0);
 
-        double delta = 0.001;
+        double delta = 0.0001;
 
         assertEquals(actualResult.getRe(), expectedResult.getRe(), delta);
         assertEquals(actualResult.getIm(), expectedResult.getIm(), delta);
     }
 
     @Test
-    public void ComplexMinusPositiveReImTest() {
+    public void complexMinusPositiveReImTest() {
         Complex firstVariable = new Complex(4.976, 9.937);
-        Complex secondVariable = new Complex( 6.119, -11.248);
+        Complex secondVariable = new Complex(6.119, -11.248);
+
         Complex actualResult = firstVariable.minus(secondVariable);
 
         Complex expectedResult = new Complex(-1.143, 21.185);
 
-        double delta = 0.001;
+        double delta = 0.0001;
 
         assertEquals(actualResult.getRe(), expectedResult.getRe(), delta);
         assertEquals(actualResult.getIm(), expectedResult.getIm(), delta);
     }
 
     @Test
-    public void ComplexMinusNegativeReImTest() {
+    public void complexMinusNegativeReImTest() {
         Complex firstVariable = new Complex(-17.483, -34.821);
         Complex secondVariable = new Complex( -19.849, 19.894);
+
         Complex actualResult = firstVariable.minus(secondVariable);
 
         Complex expectedResult = new Complex(2.366, -54.715);
 
-        double delta = 0.001;
+        double delta = 0.0001;
 
         assertEquals(actualResult.getRe(), expectedResult.getRe(), delta);
         assertEquals(actualResult.getIm(), expectedResult.getIm(), delta);
     }
 
     @Test
-    public void ComplexMinusPositiveNegativeReImTest() {
+    public void complexMinusPositiveReNegativeImTest() {
         Complex firstVariable = new Complex(22.792, -27.484);
-        Complex secondVariable = new Complex( -14.914, -22.373);
+        Complex secondVariable = new Complex(-14.914, -22.373);
+
         Complex actualResult = firstVariable.minus(secondVariable);
 
         Complex expectedResult = new Complex(37.706, -5.111);
 
-        double delta = 0.001;
+        double delta = 0.0001;
 
         assertEquals(actualResult.getRe(), expectedResult.getRe(), delta);
         assertEquals(actualResult.getIm(), expectedResult.getIm(), delta);
     }
 
     @Test
-    public void ComplexMinusNegativePositiveReImTest() {
+    public void complexMinusNegativeRePositiveImTest() {
         Complex firstVariable = new Complex(-39.293, 42.247);
-        Complex secondVariable = new Complex( 59.305, 25.962);
+        Complex secondVariable = new Complex(59.305, 25.962);
+
         Complex actualResult = firstVariable.minus(secondVariable);
 
         Complex expectedResult = new Complex(-98.598, 16.285);
 
-        double delta = 0.001;
+        double delta = 0.0001;
 
         assertEquals(actualResult.getRe(), expectedResult.getRe(), delta);
         assertEquals(actualResult.getIm(), expectedResult.getIm(), delta);
