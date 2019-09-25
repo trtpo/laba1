4eb5297e3838395e749abfbeff2ad8065a96a5c8
KozachenkoKirill750505 <kirill123211@gmail.com>
2019-09-26 00:21:00 +0300

    1st task

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.RED;
         }
         return colors[count];
     }
