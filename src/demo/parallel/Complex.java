2affdab248fb0b8567bf95bc6c978ae1d026917e
Silentium14 <silentiumslava@gmail.com>
2019-09-26 08:52:30 +0300

    add new operations

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -72,6 +72,17 @@ public class Complex {
     }
 
     /**
+     * Subtraction operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+    
+    /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
@@ -83,6 +94,44 @@ public class Complex {
         re = real;
         im = imag;
         return this;
+    }
+
+    /**
+     * Division operation.
+     * @param  b dividend
+     * @return this Complex object whose value is (this / b)
+     */
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im);
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
+     * Sinus operation
+     * @return Complex object whose value is sin(this)
+     */
+    public Complex sin() {
+        double real = Math.sin(re) * Math.cosh(im);
+        double imag = Math.cos(re) * Math.sinh(im);
+        re = real;
+        im = imag;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Cosine operation
+     * @return Complex object whose value is cos(this)
+     */
+    public Complex cos() {
+        double real = Math.cos(re) * Math.cosh(im);
+        double imag = -Math.sin(re) * Math.sinh(im);
+        re = real;
+        im = imag;
+        return new Complex(real, imag);
     }
 
     /**
