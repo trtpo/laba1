3131cd2a5e138415b6f59bfab679112e81dc69b5
Lina Loyko <linaloyko@icloud.com>
2019-09-29 17:13:52 +0300

    Added new operations: minus, devide

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,31 @@ public class Complex {
     }
 
     /**
+     * Minus operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is this - b
+     */
+    public Complex divider(Complex b){
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
+     * Devide operation.
+     * @param b divider
+     * @return this Complex object whose value is this / b
+     */
+    public Complex devide(Complex b){
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re * b.re + b.im * b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(b.re * b.re + b.im * b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
