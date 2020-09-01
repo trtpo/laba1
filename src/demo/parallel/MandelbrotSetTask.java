f0f6b035ac8093c2b1031094e684d505a4cfb276
Stanilav Zholud <63405589+MiloshTen@users.noreply.github.com>
2020-09-02 00:09:24 +0300

    Change colore

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).plus(comp).times(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -352,9 +352,9 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.BLUE,
             Color.WHITE,
-            Color.RED,
+            Color.BROWN,
             Color.rgb(100, 0, 0),
             Color.RED,
             Color.rgb(50, 0, 0)
