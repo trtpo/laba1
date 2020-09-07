76fb0458befc7974892040e26f421f99d900e41d
Durko_Anton <an7on_01@mail.ru>
2020-09-07 11:13:50 +0300

    Added test method cos(z)

--- test/ComplexTest.java
+++ test/ComplexTest.java
@@ -15,6 +15,8 @@ class ComplexTest {
 
     @org.junit.jupiter.api.Test
     void getCos() {
+        assertTrue(new Complex(Math.PI /6,0).getCos().equals(new Complex(Math.sqrt(3)/2,0)),"Test for simple cos(z) with arg = PI/6");
+        assertTrue(new Complex(2*Math.PI/3 ,0).getCos().equals(new Complex(-0.5,0)),"Test for simple cos(z) with arg =2PI/3");
     }
 
     @org.junit.jupiter.api.Test
