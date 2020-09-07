b4e4f9e7613f3a9733049f9264820d173980e585
Durko_Anton <an7on_01@mail.ru>
2020-09-07 23:18:21 +0300

    Changed test method divide, added producing exceptions

--- test/ComplexTest.java
+++ test/ComplexTest.java
@@ -16,15 +16,26 @@ class ComplexTest {
 
     @org.junit.jupiter.api.Test
     void divide() {
-        assertTrue(new Complex(1.5,1.6).divide(new Complex(0.7,3.4)).equals(new Complex((double)649/1205,-(double)398/1205)), "Test for simple division with the same signs.");
-        assertTrue(new Complex(2.3,1.1).divide(new Complex(-10.7,3.4)).equals(new Complex(-(double)2087/12605,-(double)1959/12605)), "Test for simple division with the different signs #1.");
-        assertTrue(new Complex(-3.4,0.9).divide(new Complex(3.9,-1.7)).equals(new Complex(-(double)1479/1810,-(double)227/1810)), "Test for simple division with the different signs #2.");
-        assertTrue(new Complex(2.7,-1.3).divide(new Complex(3.3,2.1)).equals(new Complex((double)103/255,-(double)166/255)), "Test for simple division with the different signs #3.");
 
-        assertTrue(new Complex(19.8,0).divide(new Complex(23.87,-39.21)).equals(new Complex((double)472626/2107201,(double)776358/2107201)), "Test for division with one value =0 #1.");
-        assertTrue(new Complex(0,1.37).divide(new Complex(5.87,4.21)).equals(new Complex((double)57677/521810 ,(double)80419/521810 )), "Test for division with one value =0 #2.");
-        assertTrue(new Complex(2,1.84).divide(new Complex(5.87,0)).equals(new Complex((double)200/587,(double)184/587)), "Test for division with one value =0 #3.");
-        assertTrue(new Complex(4.1,1.56).divide(new Complex(0,3.1)).equals(new Complex((double)78/155,-(double)41/31)), "Test for division with one value =0 #4.");
+        final Complex numberToTest_1 = new Complex(5.6,12.4);
+        final Complex numberToTest_2 = new Complex(4.7,-11.1);
+        final Complex numberToTest_3 = new Complex(-4.9,0);
+        final Complex numberToTest_4 = new Complex(0,-1.70);
+        final Complex numberToTest_5 = new Complex(0,0);
+
+        assertTrue(numberToTest_1.clone().divide(numberToTest_2).equals(new Complex(-(double)5566/7265, (double)6022/7265)),
+                "Test for simple division with the same signs.");
+        assertTrue(numberToTest_1.clone().divide(numberToTest_3.clone()).equals(new Complex(-(double)8/7, -(double)124/49)),
+                "Test for simple division with the same signs.");
+        assertTrue(numberToTest_3.clone().divide(numberToTest_1.clone()).equals(new Complex(-(double)343/2314,(double)1519/4628)),
+                "Test for simple division with the different signs #2.");
+
+        assertTrue(numberToTest_4.clone().divide(numberToTest_1 ).equals(new Complex(-(double)527/4628,-(double)119/2314)),
+                "Test for simple division with the different signs #2.");
+
+        thrown.expect(ArithmeticException.class);
+        thrown.expectMessage("Error!Division by 0.");
+        numberToTest_1.divide(numberToTest_5);
     }
 
     @org.junit.jupiter.api.Test
