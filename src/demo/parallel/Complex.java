1e17bf3ae6d98739c4b584bd34433ae956568150
VladLinkevich <linkevichvv@mail.ru>
2020-09-07 14:55:43 +0300

    Add new complex operations

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,12 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
@@ -82,6 +88,12 @@ public class Complex {
         double imag = a.re * b.im + a.im * b.re;
         re = real;
         im = imag;
+        return this;
+    }
+
+    public Complex divide(Complex b) {
+        re = (re * b.re + im * b.im)/(b.re * b.re + b.im * b.im);
+        im = (im * b.re - re * b.im)/(b.re * b.re + b.im * b.im);
         return this;
     }
 
