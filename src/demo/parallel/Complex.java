e333f3249e1367bc56d30613b74d511e3539d3f8
Durko_Anton <an7on_01@mail.ru>
2020-09-07 09:04:33 +0300

    Added the operation subtraction of complex numbers

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -72,6 +72,17 @@ public class Complex {
     }
 
     /**
+     * Subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
