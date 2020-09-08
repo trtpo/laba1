152d929c067d4fc60914187e9c18c234ea7df760
Durko_Anton <an7on_01@mail.ru>
2020-09-08 18:24:31 +0300

    Changed error message of test methods and changed them names(added check)

--- test/ComplexTest.java
+++ test/ComplexTest.java
@@ -7,15 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class ComplexTest {
 
     @Rule
- public  ExpectedException thrown = ExpectedException.none();
+    public  ExpectedException thrown = ExpectedException.none();
 
     @org.junit.jupiter.api.Test
-    void minus() {
-        assertTrue(new Complex(10.3,2.1).minus(new Complex(7.9,-4)).equals(new Complex(2.4,6.1)), "Test for simple subtraction #1.");
+    void checkMinus() {
+        assertTrue(new Complex(10.3,2.1).minus(new Complex(7.9,-4)).equals(new Complex(2.4,6.1)),
+                "Test for simple subtraction.");
     }
 
     @org.junit.jupiter.api.Test
-    void divide() {
+    void checkDivide() {
 
         final Complex numberToTest_1 = new Complex(5.6,12.4);
         final Complex numberToTest_2 = new Complex(4.7,-11.1);
@@ -23,15 +24,16 @@ class ComplexTest {
         final Complex numberToTest_4 = new Complex(0,-1.70);
         final Complex numberToTest_5 = new Complex(0,0);
 
-        assertTrue(numberToTest_1.clone().divide(numberToTest_2).equals(new Complex(-(double)5566/7265, (double)6022/7265)),
-                "Test for simple division with the same signs.");
+        assertTrue(numberToTest_1.clone().divide(numberToTest_2.clone()).equals(new Complex(-(double)5566/7265, (double)6022/7265)),
+                "Test for division of complex number with not null re and im.");
         assertTrue(numberToTest_1.clone().divide(numberToTest_3.clone()).equals(new Complex(-(double)8/7, -(double)124/49)),
-                "Test for simple division with the same signs.");
+                "Test for division by number with im = 0.");
+        assertTrue(numberToTest_2.clone().divide(numberToTest_4.clone()).equals(new Complex((double)111/17, (double)47/17)),
+                "Test for division by number with re = 0.");
         assertTrue(numberToTest_3.clone().divide(numberToTest_1.clone()).equals(new Complex(-(double)343/2314,(double)1519/4628)),
-                "Test for simple division with the different signs #2.");
-
+                "Test for division number with im = 0.");
         assertTrue(numberToTest_4.clone().divide(numberToTest_1 ).equals(new Complex(-(double)527/4628,-(double)119/2314)),
-                "Test for simple division with the different signs #2.");
+                "Test for division number with re = 0.");
 
         thrown.expect(ArithmeticException.class);
         thrown.expectMessage("Error!Division by 0.");
@@ -39,13 +41,15 @@ class ComplexTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getCos() {
-        assertTrue(new Complex(Math.PI /6,0).getCos().equals(new Complex(Math.sqrt(3)/2,0)),"Test for simple cos(z) with arg = PI/6");
-        assertTrue(new Complex(2*Math.PI/3 ,0).getCos().equals(new Complex(-0.5,0)),"Test for simple cos(z) with arg =2PI/3");
+    void checkGetCos() {
+        assertTrue(new Complex(Math.PI /6,0).getCos().equals(new Complex(Math.sqrt(3)/2,0)),
+                "Test for calculating cos(z) with arg = PI/6");
+        assertTrue(new Complex(2*Math.PI/3 ,0).getCos().equals(new Complex(-0.5,0)),
+                "Test for calculating cos(z) with arg =2PI/3");
     }
 
     @org.junit.jupiter.api.Test
-    void pow() {
+    void checkPow() {
         final Complex numberToTest_1 = new Complex(5.6,12.4);
         final Complex numberToTest_2 = new Complex(-5.6,-12.4);
         final Complex numberToTest_3 = new Complex(5.6,-12.4);
@@ -56,7 +60,7 @@ class ComplexTest {
         assertTrue(numberToTest_3.pow(0).equals(new Complex(1,0)), "Test for degree 0.");
 
         thrown.expect(ArithmeticException.class);
-        thrown.expectMessage("Error");
+        thrown.expectMessage("Error!Division by 0.");
         numberToTest_4.pow(1);
     }
 }
