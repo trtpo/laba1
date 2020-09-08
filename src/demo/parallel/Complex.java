a748946fd69e11b79087b270e71a0848eb5215b1
Vadim Taras <tarasskr34@gmail.com>
2020-09-08 17:50:13 +0300

    iznemenie fraktala

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,21 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b)
+    {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = a.re / b.re - a.im / b.im;
+        double imag = a.re / b.im + a.im / b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
     /**
      * Multiply operation.
      * @param  b multiplier
