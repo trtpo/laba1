4d4889132e7424b37706761838d07447a014903e
Pobol Maxim <maxim.pobol@gmail.com>
2020-09-08 13:27:24 +0300

    color

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(141, 61, 128),
+            Color.LIGHTPINK,
             Color.WHITE,
-            Color.RED,
+            Color.PURPLE,
             Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.BLUE,
+            Color.rgb(10, 77, 195)
         };
         
         /**
