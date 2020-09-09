283c2dcaec1549b06fa018dae9f4b363b18e58a2
Stanislau Matskevich <st.matskevich.work@gmail.com>
2020-09-09 10:07:43 +0300

    Added more tests for minus operations of Complex class

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -7,11 +7,27 @@ import org.junit.Test;
 public class ComplexTest {
 
 	@Test
-	public void testMinus() {
+	public void testMinusBothPositive() {
         Complex a = new Complex(7, 6);
         Complex b = new Complex(1, 5);
         Complex result = a.minus(b);
         assertEquals(new Complex(6, 1), result);
+	}
+	
+	@Test
+	public void testMinusBothNegative() {
+        Complex a = new Complex(-7, -6);
+        Complex b = new Complex(-1, -5);
+        Complex result = a.minus(b);
+        assertEquals(new Complex(-6, -1), result);
+	}
+	
+	@Test
+	public void testMinusResultZero() {
+        Complex a = new Complex(7, 6);
+        Complex b = new Complex(7, 6);
+        Complex result = a.minus(b);
+        assertEquals(new Complex(0, 0), result);
 	}
 
 }
