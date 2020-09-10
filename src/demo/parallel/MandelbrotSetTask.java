0e1d5fdbf843ad26fce66e534d450156df059ad0
Константин Лосевич (850502) <62259384+Konstantsiy@users.noreply.github.com>
2020-09-10 21:32:45 +0300

    Update MandelbrotSetTask.java

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.sin().sub(comp); // c = sin(c)^2-comp
+            c = c.sin().times(c).sub(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
