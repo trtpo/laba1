3dfac20b8d2943e264a850933efbb3b6b3f095b7
Chervyak <chervyak375@gmail.com>
2020-09-11 22:49:44 +0300

    Change the color scheme of the image.

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(40, 40, 0),
+            Color.YELLOW,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.YELLOW,
+            Color.rgb(100, 100, 0),
+            Color.YELLOW,
+            Color.rgb(50, 50, 0)
         };
         
         /**
