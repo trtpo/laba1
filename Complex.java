51ff9cb1738510c5dbe2df81aaae0fcd4ef8dd9c
Лимонтова Мария 850501 <62797799+limontova@users.noreply.github.com>
2020-09-13 21:27:00 +0300

    Add files via upload
    добавлены unit тесты

--- Complex.java
+++ Complex.java
@@ -49,6 +49,14 @@ public class Complex {
     private double re;   // the real part
     private double im;   // the imaginary part
 
+    public double getIm() {
+        return im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
     /** 
      * create a new object with the given real and imaginary parts
      * 
@@ -91,15 +99,7 @@ public class Complex {
         return this;
     }
 
-    public Complex reciprocal() {
-        double scale = re*re + im*im;
-        return new Complex(re / scale, -im / scale);
-    }
 
-    public Complex divides(Complex b) {
-        Complex a = this;
-        return a.times(b.reciprocal());
-    }
 
     public Complex cos(Complex b) {
         re = Math.cos(b.re) * Math.cosh(b.im);
@@ -107,14 +107,13 @@ public class Complex {
         return this;
     }
 
-    public Complex scale(double alpha) {
-        re = alpha * re;
-        im = alpha * im;
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re - b.re;
+        double imag = a.im - b.im;
+        re = real;
+        im = imag;
         return this;
-    }
-
-    public Complex tan(Complex b) {
-        return sin(b).divides(cos(b));
     }
 
     /**
