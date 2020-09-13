472123bd96bba772f430631958e08e6bc61da156
qkay111 <qkay111@gmail.com>
2020-09-13 16:58:18 +0300

    Added minus operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,15 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        re = real;
+        im = imag;
+        return this;
+    }
+
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
