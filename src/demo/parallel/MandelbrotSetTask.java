ce9c7200c7089379822a51f36ea02a2ab5a2cf2a
Diman <vector_diman@mail.ru>
2020-09-13 17:14:33 +0300

    formula changed (version 2.0)

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.plus(comp).plus(c).times(c).times(comp);
+            c = c.plus(c).sin().exp().times(comp).scale(1.1);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
