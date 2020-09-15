64e9274c209941d2de3b9f7145ab11b04c434a83
Anton But-Gusaim(850505) <a.butgusaim1@mail.ru>
2020-09-15 22:30:27 +0300

    New operation and change fractal function

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,7 +274,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).times(c).plus(comp);
+            c = c.cube().minus(comp).divide(comp).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
