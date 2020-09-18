338b5720e0a6dbe7f33300c3534d6f06ae735514
unknown <andrei.karymau@gmail.com>
2020-09-18 22:35:16 +0300

    Z(n+1)=Z(n)^2 + c has been replaced with Z(n+1)=Z(n)^3 + c

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -273,9 +273,9 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private int calc(Complex comp) {
         int count = 0;
-        Complex c = new Complex(0, 1);
+        Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.pow(4).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
