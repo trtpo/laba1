ee8a4cdc7d00431d7adaad0c1f1402c4b8a57b03
Vorobeyyyyyy <mrskilkchat@gmail.com>
2020-09-18 23:49:45 +0300

    Unit tests

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -106,4 +106,20 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+
+    public double getRe() {
+        return re;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setIm(double im) {
+        this.im = im;
+    }
 }
