9abab6bb5db694542e041c8ea63f26ea59f8a2d5
Иван <ivan@MacBook-Pro-Ivan.local>
2020-09-18 14:00:48 +0300

    added operation and fractal equation changed

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,12 @@ public class Complex {
         return this;
     }
 
+    public Complex divide(Complex b) {
+        re = (re * b.re + im * b.im) / (b.re * b.re + b.im * b.im);
+        im = (im * b.re - re * b.im) / (b.re * b.re + b.im * b.im);
+        return this;
+    }
+
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
