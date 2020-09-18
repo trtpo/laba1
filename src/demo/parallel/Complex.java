e13ad9b83b68ca519ee97c9f631f1842603ca932
Levkostychenko <lev.kostychenko@gmail.com>
2020-09-18 22:52:23 +0300

    Add math operations

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,33 @@ public class Complex {
         return this;
     }
 
+    /**
+     * Substract operation.
+     * @param b residual
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
+     * Division operation
+     * @param b divisor
+     * @return this Complex object whose value is (this/b).
+     * If b == 0
+     * @return this Complex object
+     */
+    public Complex divide(Complex b) {
+        if ((b.re*b.re+b.im*b.im) != 0) {
+            re = (re * b.re + im * b.im) / (b.re * b.re + b.im * b.im);
+            im = (im * b.re + re * b.im) / (b.re * b.re + b.im * b.im);
+            return this;
+        }
+        else return this;
+    }
+
     @Override
     public boolean equals(Object obj) {
         try {
