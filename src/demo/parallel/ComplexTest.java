f27a46b285b250efc68998348d4eb19a9f685210
stroitel <oleg.vizgalov@gmail.com>
2020-09-19 04:17:19 +0300

    Added unit tests

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,28 @@
+package demo.parallel;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ComplexTest extends Assert {
+    @Test
+    public void minusTest(){
+        Complex a = new Complex(10.2, 5.5);
+        Complex b = new Complex(-16.7, 3.1);
+        assertNotNull(a.minus(b));
+    }
+
+    @Test
+    public void cubeTest(){
+        Complex a = new Complex(10.2, 5.5);
+        assertNotNull(a.cube());
+    }
+
+    @Test
+    public void specialTest(){
+        Complex a = new Complex(10.2, 5.5);
+        Complex b = new Complex(-16.7, 3.1);
+        assertNotNull(a.special(b));
+    }
+
+}
