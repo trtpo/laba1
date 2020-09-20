ab2fe5d507bbc08569ea806e58857daea2f72742
Kirill <kirillius2016@gmail.com>
2020-09-20 23:01:50 +0300

    Added mathematical sin operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -86,6 +86,16 @@ public class Complex {
     }
 
     /**
+     * sin operation.
+     * @return this Complex object whose value is sin(this)
+     */
+    public Complex sin() {
+        re = Math.sin(re) * Math.cosh(im);
+        im = Math.cos(re) * Math.sinh(im);
+        return this;
+    }
+
+    /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
      * @return square of length
