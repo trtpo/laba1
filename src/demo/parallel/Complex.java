5eefc0a408f4b418e71c99bef9bf7dc4a00bf21b
denis-850505 <denis14philimoncik@gmail.com>
2020-10-20 21:49:10 +0300

    Add some methods

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,21 @@ public class Complex {
         return this;
     }
 
+    public Complex sub(Complex b) {
+        re-=b.re;
+        im-=b.im;
+        return this;
+    }
+
+    public Complex div (Complex b)
+    {
+        double real = ( re * b.re + im * b.im)/( b.re * b.re + b.im * b.im);
+        double imag = ( im * b.re - b.im * re)/( b.re * b.re + b.im * b.im);
+        re=real;
+        im=imag;
+        return this;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
