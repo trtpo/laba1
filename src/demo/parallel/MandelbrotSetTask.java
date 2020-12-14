7c11c3ea5ae368c2a0b2b7eb1f66609b6ea86c16
Ilia1215 <65759907+Ilia1215@users.noreply.github.com>
2020-12-14 20:42:45 +0300

    Change color

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -39,10 +39,7 @@ import javafx.scene.paint.Color;
 
 
 /**
- * Task to render Mandelbrot set using given parameters. See {@link 
- * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
- * double, double, double, double, double, double, double, double, boolean) 
- * constructor} for parameters list. The task returns time in milliseconds as 
+ * Task to render Mandelbrot set using given parameters. See  for parameters list. The task returns time in milliseconds as
  * its calculated value.
  * 
  * <p><i>
@@ -352,9 +349,9 @@ class MandelbrotSetTask extends Task<Long> {
          */
         Color[] cc = {
             Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
+            Color.BLUEVIOLET,
+            Color.WHITESMOKE,
+            Color.AZURE,
             Color.rgb(100, 0, 0),
             Color.RED,
             Color.rgb(50, 0, 0)
