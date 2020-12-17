ade0a97f7bac6a826e1cecd2c04df7826fe3516e
Бурень Сергей 850505 <sergei.buren@gmail.com>
2020-12-17 17:56:23 +0300

    Fractal equation has been changed

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,14 @@ public class Complex {
         return this;
     }
 
+    public Complex sqr(){
+        Complex a = this;
+        double real = a.re * a.re - a.im * a.im;
+        double imag = a.re * a.im + a.im * a.re;
+        re = real;
+        im = imag;
+        return this;
+    }
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
