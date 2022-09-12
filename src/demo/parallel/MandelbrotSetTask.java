d9beb753db0b02b2c151c89e391ecd08edc0b2bb
Dmitry Kudryavcev <dimitrukdr@gmail.com>
2022-09-12 23:25:39 +0300

    Changed color theme

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.GOLD;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.DARKCYAN,
+            Color.CYAN,
             Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.LIME,
+            Color.MEDIUMSEAGREEN,
+            Color.OLIVE,
+            Color.GREEN
         };
         
         /**
