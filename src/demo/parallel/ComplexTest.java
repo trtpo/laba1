fc7f5960591648499d6ffdedca7ad59d95e899ec
Pavel Vaitsiakhovich 030501 <pavel03.03@inbox.ru>
2022-10-04 11:09:08 +0300

    Junit Tests

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,20 @@
+package demo.parallel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplexTest {
+
+    @Test
+    void plus() {
+    }
+
+    @Test
+    void times() {
+    }
+
+    @Test
+    void lengthSQ() {
+    }
+}
