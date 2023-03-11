e6513969c6cdd71bc621c5073ed8262e0ecb63cd
HlebK 050541 <h.kokhanovsky@gmail.com>
2023-03-11 18:52:16 +0300

    The class Complex was updated with the methods: equals, minus, devide. Those methods were used for refactoring the fractal equation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -93,4 +93,35 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+
+    public boolean equals(Complex b) {
+        return this.im==b.im && this.re ==b.re;
+    }
+
+    /**
+     * Sub operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
+     * Divide operation.
+     * @param  b divider
+     * @return this Complex object whose value is this / b
+     */
+
+    public Complex divide(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re*b.re+b.im*b.im);
+        double imag = (a.re * b.im - a.re * b.im)/(b.re*b.re+b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
 }
--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -276,7 +276,7 @@ class MandelbrotSetTask extends Task<Long> {
 
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp).plus(c).times(c);
+            c = c.times(c).plus(comp).times(comp.divide(c)).minus(comp.minus(c));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
