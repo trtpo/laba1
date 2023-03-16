7c310555ab946d8c61537719e621085806acf41b
DaniilSokolov050541 <sokoloffdan@gmail.com>
2023-03-17 00:46:40 +0300

    Change the fractal equation yc(z) = z^2 +c to the same (same operations + and *)

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex b = new Complex(0.2, 3);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).times(comp).times(b).times(c).plus(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
