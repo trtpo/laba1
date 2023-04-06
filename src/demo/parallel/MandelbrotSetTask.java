3536e48fa70ecdc71ff382f17d3f83a45c8c6e42
Syomin 050541 <28.12.1992.daniil@gmail.com>
2023-04-06 20:33:10 +0300

    использование операций из предыдущего комvита, т.к. забыл

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,10 +274,10 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
-        Complex b = new Complex(0.4, 1);
+        Complex b = new Complex(-0.1, 0.5);
 
         do {
-            c = c.times(c).plus(comp).times(comp).times(b).plus(c);
+            c = c.times(c.times(c.times(c))).div(b).sub(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
