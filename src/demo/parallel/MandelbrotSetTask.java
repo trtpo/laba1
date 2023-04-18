2e70bde232f7393f3f10dfb9c48d2f02861360a5
Artsem <artiomzaharenko@gmail.com>
2023-04-18 12:31:43 +0300

    implemented new operations

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -278,7 +278,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).times(c).plus(comp);
+            c = c.times(comp).plus(comp).sub(comp.divide(c));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
