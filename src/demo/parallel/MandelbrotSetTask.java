d030cc1c0adafde22e892816e987568db45f8f44
Aliaksandra Leikina <lexale.wiw@gmail.com>
2023-05-28 21:14:14 +0300

    New_fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex b = new Complex(0.1, 2);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).plus(comp).sub(b).times(b).div(b).plus(c);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
