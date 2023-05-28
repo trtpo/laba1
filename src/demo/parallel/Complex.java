9ce3fa6db2fb1a6eab8e617fdcad9f584543f6aa
Lufflee-Vaflee <55928021+Lufflee-Vaflee@users.noreply.github.com>
2023-05-28 18:15:39 +0300

    few more complex funtions

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -66,6 +66,18 @@ public class Complex {
         return this;
     }
 
+    public Complex div(Complex b)throws ArithmeticException {
+        if (b.lengthSQ() == 0) {
+            throw new ArithmeticException("Error! Division by zero");
+        }
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im);
+        double imag = (b.re * a.im - b.im * a.re);
+        re = real / b.lengthSQ() ;
+        im = imag / b.lengthSQ() ;
+        return this;
+    }
+
     /**
      * Add operation.
      * @param b summand
@@ -98,5 +110,13 @@ public class Complex {
     */
     public double lengthSQ() {
         return re * re + im * im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
     }
 }
