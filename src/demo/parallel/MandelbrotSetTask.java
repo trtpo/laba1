76700ac66524d9459ac56c60a993a60102b34445
fathergeorgii217rc@gmail.com <fathergeorgii217rc@gmail.com>
2023-09-01 16:17:38 +0300

    colors palette changed

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.rgb(187, 135, 240),
+                Color.rgb(185, 63, 41),
+                Color.rgb(150, 73, 99),
+                Color.rgb(194, 28, 31),
+                Color.rgb(71, 99, 59),
+                Color.rgb(101, 214, 25),
+                Color.rgb(200, 104, 11),
         };
         
         /**
