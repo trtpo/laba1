92bab2a69e8779965a1d2ca31d4fb0dcff91269a
Eduard Kaliaha <200427816eduard@gmail.com>
2023-09-02 19:43:33 +0300

    color scheme commit

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -310,9 +310,9 @@ class MandelbrotSetTask extends Task<Long> {
         for (int i = 0; i < ANTIALIASING_BASE; i++) {
             for (int j = 0; j < ANTIALIASING_BASE; j++) {
                 Color c = calcPixel(x + step * (i + 0.5) - 0.5, y + step * (j + 0.5) - 0.5);
-                r += c.getRed() / N;
-                g += c.getGreen() / N;
-                b += c.getBlue() / N;
+                r += c.getGreen() / N;
+                g += c.getBlue() / N;
+                b += c.getRed() / N;
             }
         }
         return new Color(clamp(r), clamp(g), clamp(b), 1);
