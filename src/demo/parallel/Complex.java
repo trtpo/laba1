1b50c7ad570465fe520110df02702716f931f217
h4thqewjtch <h4thqewjtch@gmail.com>
2023-09-05 13:07:13 +0300

    tests

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -99,4 +99,12 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
 }
