f6be98778b2e25476b8e20e102c3950702449edd
safertao <safertao@gmail.com>
2023-09-05 11:30:28 +0300

    fix: change color scheme of fractal

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
+            Color.rgb(100, 200, 33),
             Color.RED,
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
+            Color.rgb(30, 64, 127),
             Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(50, 88, 20)
         };
         
         /**
