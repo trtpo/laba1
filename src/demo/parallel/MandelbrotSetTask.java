12054a7eeb6e0c936d867ff45bd3ed0e77006ec8
semen <mish_semenov@mail.ru>
2023-09-07 10:30:08 +0300

    first commit information: change color in application

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.CYAN;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(70, 20, 0),
+            Color.GREEN,
+            Color.YELLOW,
+            Color.BLUE,
+            Color.rgb(100, 20, 25),
+            Color.ORANGE,
+            Color.rgb(50, 20, 50)
         };
         
         /**
