24152e0ac7eeb713b6a5d2b474c4eca8dc7641d1
Qurcaivel <alexinq6@gmail.com>
2023-09-07 10:18:42 +0300

    C: Added new Complex class operations and their use in modified fractal equation

--- src/java/demo/parallel/MandelbrotSetTask.java
+++ src/java/demo/parallel/MandelbrotSetTask.java
@@ -274,9 +274,8 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
-        Complex k = new Complex(0.1, 0);
         do {
-            c = c.times(c).times(c).times(c).times(k).plus(comp);
+            c = c.lazyPower(4).times(0.1).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
