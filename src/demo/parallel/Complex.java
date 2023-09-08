e90e1b4b0f623925bebd0e452268c60159b8e532
Kirill Satsuta <satsuta.k@mail.ru>
2023-09-08 18:38:59 +0300

    Add minus and div

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -93,4 +93,18 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im) ;
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im) ;
+        re = real;
+        im = imag;
+        return this;
+    }
 }
