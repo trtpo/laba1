82a4237eb4afcda00030f4dd842584708ade1c56
maybesomesense <228cacka228@mail.ru>
2023-09-12 01:09:11 +0300

    third commit

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -356,7 +356,7 @@ class MandelbrotSetTask extends Task<Long> {
             Color.WHITE,
             Color.WHITE,
             Color.rgb(100, 0, 0),
-            Color.RED,
+            Color.PURPLE,
             Color.rgb(50, 0, 0)
         };
         
