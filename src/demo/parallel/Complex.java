4f032b0349ecc272a8557c1f51d87883319cd6fe
RomanFedenko130501 <romachkafedenko2003@mail.ru>
2023-09-11 14:52:03 +0300

    Added Unit-tests for check running new operations

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -60,6 +60,15 @@ public class Complex {
         im = imag;
     }
 
+    public double getRe()
+    {
+        return this.re;
+    }
+
+    public double getIm()
+    {
+        return this.im;
+    }
     /**
      * Add operation.
      * @param b summand
@@ -78,9 +87,9 @@ public class Complex {
         return this;
     }
 
-    public Complex mul(Integer b) {
-        re *=b;
-        im *=b;
+    public Complex offset(double b) {
+        re +=b;
+        im +=b;
         return this;
     }
     /**
---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,29 @@
+package demo.parallel;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ComplexTest {
+     @Test
+     public void minusTest(){
+          Complex a=new Complex(4.4,5.5);
+          Complex b=new Complex(-3.4,6.7);
+
+          Complex rez=a.minus(b);
+
+          assertEquals(4.4-(-3.4),rez.getRe(),0.0001);
+          assertEquals(5.5-6.7,rez.getIm(),0.0001);
+     }
+
+     @Test
+     public void mulTest(){
+          Complex a=new Complex(4.4,5.5);
+          double argum = 0.3;
+          Complex rez=a.offset(argum);
+
+          assertEquals(4.4+0.3,rez.getRe(),0.0001);
+          assertEquals(5.5+0.3,rez.getIm(),0.0001);
+     }
+
+}
