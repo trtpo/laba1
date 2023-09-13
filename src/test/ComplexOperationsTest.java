2042240cde1685b76daa843464df62c0fc7d98be
TUF 15 <tasyap03@mail.ru>
2023-09-13 20:59:55 +0300

    Unit tests

---
+++ src/test/ComplexOperationsTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import demo.parallel.Complex;
+
+public class ComplexOperationsTest {
+    @Test
+    public void testDivide() {
+        Complex x = new Complex(2, 1);
+        Complex y = new Complex(1, 2);
+        Assert.assertTrue("Expected true, but got false", new Complex(0.8, -0.6).equals(x.div(y), 0.001));
+        x = new Complex(1, 2);
+        y = new Complex(2, 1);
+        Assert.assertTrue("Expected true, but got false", new Complex(0.8, 0.6).equals(x.div(y), 0.001));
+        x = new Complex(0, 0);
+        y = new Complex(2, 2);
+        Assert.assertTrue("Expected true, but got false", new Complex(0.0, 0.0).equals(x.div(y), 0.001));
+    }
+
+    @Test
+    public void testSubtract() {
+        Complex x = new Complex(2, 1);
+        Complex y = new Complex(1, 2);
+        Assert.assertTrue("Expected true, but got false", new Complex(1, -1).equals(x.sub(y), 0.0));
+        x = new Complex(1, 2);
+        y = new Complex(2, 1);
+        Assert.assertTrue("Expected true, but got false", new Complex(-1, 1).equals(x.sub(y), 0.0));
+        x = new Complex(0, 0);
+        y = new Complex(2, 2);
+        Assert.assertTrue("Expected true, but got false", new Complex(-2, -2).equals(x.sub(y), 0.0));
+    }
+}
