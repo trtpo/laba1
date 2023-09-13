8ac64c237f3646f846b9d289d948846796698ee8
peabody28 <gorbachewski.m@gmail.com>
2023-09-13 09:24:00 +0300

    color changes

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.AQUA;
         }
         return colors[count];
     }
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
+            Color.rgb(55, 15, 80),
+            Color.BLUE,
             Color.WHITE,
+            Color.BLUE,
+            Color.rgb(189, 101, 234),
             Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(208, 168, 250)
         };
         
         /**
