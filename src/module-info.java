7136fa2d87bd1f2f36808c78e2ebe302070b9a28
MikhailShychko <mihalshichko@gmail.com>
2023-09-14 21:10:54 +0300

    Colors changed

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module laba1 {
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.media;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    opens demo.parallel;
+}
