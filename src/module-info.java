480f813b2b9525ffd94d549569a4f7c024f6d4b2
rin-amari <marina.c1700@gmail.com>
2023-09-15 02:19:40 +0300

    Updated module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module trtpo.lab {
+    exports demo.parallel;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+}
