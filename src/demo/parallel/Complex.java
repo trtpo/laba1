4c8a0242c7d53b5981d2eb204049bed5e6a6aa7e
Dimalaguta <dimalaguta089@gmail.com>
2023-09-15 11:46:36 +0300

    Add minus and div and their usage

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,12 +70,24 @@ public class Complex {
         im += b.im;
         return this;
     }
-
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
     /**
      * Multiply operation.
      * @param  b multiplier
      * @return this Complex object whose value is this * b
      */
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im) ;
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im) ;
+        re = real;
+        im = imag;
+        return this;
+    }
     public Complex times(Complex b) {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
