7470d3852dad8cc4d360273b5bc2dd21cb1fb5be
Dimalaguta <dimalaguta089@gmail.com>
2023-09-15 11:50:02 +0300

    add Unit tests (minus and div)

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -59,7 +59,13 @@ public class Complex {
         re = real;
         im = imag;
     }
+    public double getRe() {
+        return re;
+    }
 
+    public double getIm() {
+        return im;
+    }
     /**
      * Add operation.
      * @param b summand
