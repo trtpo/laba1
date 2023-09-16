00487e433aa92bc4d7c1c4b9b74c3716a6bdd102
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-09-16 16:35:18 +0300

    Another variant of a changed equation, based on original operations, is added. New operations (substraction and exponentiation) are added. New equation, consisting of them, is added

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,11 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex one = new Complex(1, 0);
         do {
-            c = c.times(c).times(c).plus(comp);
+//            c = c.times(c).plus(comp);    //original equation
+//            c = c.plus(one).times(c).plus(comp);    //changed equation
+            c = c.pow(3).minus(comp);    //new equation
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
