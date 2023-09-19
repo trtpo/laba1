10fab2bc259a99a4ff9447e1c3c076f1761106b6
Bobichek <91381246+Bobichek@users.noreply.github.com>
2023-09-19 17:54:57 +0300

    new_test

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,13 @@ public class Complex {
         return this;
     }
 
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
 
     public Complex minus(Complex b) {
         re -= b.re;
