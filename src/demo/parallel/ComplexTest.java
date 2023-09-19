3ee26fa5292a72181f5af50e5def07f14c21c893
pavlikbaranov1604@gmail.com <pavlikbaranov1604@gmail.com>
2023-09-19 18:10:30 +0300

    test change

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,34 @@
+package demo.parallel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ComplexTest {
+
+    @org.junit.jupiter.api.Test
+    public void testMinus() {
+
+        Complex a = new Complex(3, 5);
+        Complex b = new Complex(1, 2);
+
+        Complex result = a.minus(b);
+
+        assertEquals(2, result.getRe());
+        assertEquals(7, result.getIm());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDiv() {
+        Complex a = new Complex(3, 5);
+        Complex b = new Complex(1, 2);
+
+        Complex result = a.div(b);
+
+        assertEquals(0.5, result.getRe());
+        assertEquals(6.5, result.getIm());
+
+    }
+
+}
+
