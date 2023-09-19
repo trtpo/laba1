1ae7231d28fb24e1b7b28d861b32feed8e6a2389
pavlikbaranov1604@gmail.com <pavlikbaranov1604@gmail.com>
2023-09-19 17:41:23 +0300

    unit change

--- src/demo/parallel/Test.java
+++ src/demo/parallel/Test.java
@@ -1,30 +1,22 @@
 package demo.parallel;
 
-import org.junit.jupiter.api.Nested;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-public class Test {
 
-    @Nested
     class ComplexTest {
 
         @org.junit.jupiter.api.Test
         public void testMinus() {
-            // Создаем комплексные числа для тестирования
+
             Complex a = new Complex(3, 5);
             Complex b = new Complex(1, 2);
 
-            // Вызываем метод minus
             Complex result = a.minus(b);
 
-            // Проверяем, что результат соответствует ожидаемым значениям
             assertEquals(2, result.getRe());
             assertEquals(7, result.getIm());
 
-            // Проверяем, что метод возвращает ссылку на текущий объект
-            assertSame(a, result);
         }
 
         @org.junit.jupiter.api.Test
@@ -37,8 +29,7 @@ public class Test {
             assertEquals(0.5, result.getRe());
             assertEquals(6.5, result.getIm());
 
-            assertSame(a, result);
         }
 
     }
-}
+
