513a61cee2bc62eb75193c6e97b2a140c97718c7
Nikita <nikki724619@gmail.com>
2023-10-06 16:29:17 +0300

    second commit

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -48,6 +48,16 @@ public class Complex {
     
     private double re;   // the real part
     private double im;   // the imaginary part
+    /**
+     * Subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
 
     /** 
      * create a new object with the given real and imaginary parts
@@ -70,7 +80,14 @@ public class Complex {
         im += b.im;
         return this;
     }
-
+    public Complex div(Complex b) {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im) ;
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im) ;
+        re = real;
+        im = imag;
+        return this;
+    }
     /**
      * Multiply operation.
      * @param  b multiplier
