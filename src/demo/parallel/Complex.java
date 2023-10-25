4aeb61b40d583e115ccb5232f8c4dff9836883db
Губаревич Агата <abobusyuoiyo@gmail.com>
2023-10-25 23:01:39 +0300

    Добавила Unit тесты

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -95,7 +95,13 @@ public class Complex {
         im = imag;
         return this;
     }
+    public double getRe() {
+        return re;
+    }
 
+    public double getIm() {
+        return im;
+    }
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
