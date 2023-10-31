49b56c3d2ad0f9307ccad329b95ea34f9dcdfce2
reroora <potom>
2023-10-31 23:37:59 +0300

    Add unit tests and fix divide operation

--- src/main/java/demo/parallel/Complex.java
+++ src/main/java/demo/parallel/Complex.java
@@ -89,11 +89,23 @@ public class Complex {
         if(b.re == 0 && b.im == 0)
             throw new IllegalArgumentException("Argument 'divisor' is 0");
         Complex a = this;
-        double real = a.re * b.re - a.im * b.im / (b.re * b.re + b.im * b.im);
-        double imag = a.im * b.re - a.re * b.im / (b.re * b.re + b.im * b.im);
+        double real = (a.re * b.re - a.im * b.im) / (b.re * b.re + b.im * b.im);
+        double imag = (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im);
         re = real;
         im = imag;
         return this;
+    }
+
+    public boolean equals(Complex b){
+        return re == b.re && im == b.im;
+    }
+
+    public double getRe(){
+        return re;
+    }
+
+    public double getIm() {
+        return im;
     }
 
     /**
--- src/main/java/demo/parallel/MandelbrotSetTask.java
+++ src/main/java/demo/parallel/MandelbrotSetTask.java
@@ -278,7 +278,7 @@ class MandelbrotSetTask extends Task<Long> {
         Complex k = new Complex(0.1, 0);
         do {
             //c = c.times(c).times(c).times(c).times(k).plus(comp);
-            c = c.times(c).times(c).times(c).times(k).divide(new Complex(5, 5)).plus(comp) ;
+            c = c.times(c).times(c).times(c).times(k).divide(new Complex(5, 0)).plus(comp) ;
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
