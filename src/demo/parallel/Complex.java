b17b3a21b0e6199d3bfcba03647a8a3ea305f596
oleg <oleg.sheverdov2004@mail.ru>
2023-10-31 22:20:57 +0300

    add tests

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -91,6 +91,21 @@ public class Complex {
         return this;
     }
 
+    public double getRe() {
+        return re;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setIm(double im) {
+        this.im = im;
+    }
     public Complex div(Complex b) {
         Complex a = this;
         double real = a.re * b.re + a.im * b.im;
