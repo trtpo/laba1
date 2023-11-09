376c63923d3ad256aa3eaaf3ca2cefcb4437d6e9
SensasBoy <vladimir_dementev_2000@mail.ru>
2023-11-09 23:39:31 +0300

    Changed the fractal equation

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(c).plus(comp).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -335,7 +335,7 @@ class MandelbrotSetTask extends Task<Long> {
      */
     private Color getColor(int count) {
         if (count >= colors.length) {
-            return Color.BLACK;
+            return Color.YELLOW;
         }
         return colors[count];
     }
